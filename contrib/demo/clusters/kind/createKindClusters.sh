f669012c9fb6880346f022713794630f81c360ed
Jason Hall <jasonhall@redhat.com>
2021-07-08 14:18:37 -0400

    Generalize createKindClusters.sh to create multiple clusters
    When given no args, it creates a us-east1 and us-west1 cluster as
    before. When given args, it creates clusters with those names. KinD
    configs, kubeconfigs and Cluster CRD YAMLs are generated and gitignored
    to avoid churn and potentially leaking sensitive information.

--- contrib/demo/clusters/kind/createKindClusters.sh
+++ contrib/demo/clusters/kind/createKindClusters.sh
@@ -1,9 +1,35 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -euxo pipefail
 
 DEMO_ROOT="$(dirname "${BASH_SOURCE}")/../.."
 
-kind create cluster --config ${DEMO_ROOT}/clusters/kind/us-east1.config --kubeconfig ${DEMO_ROOT}/clusters/kind/us-east1.kubeconfig
-sed -e 's/^/    /' ${DEMO_ROOT}/clusters/kind/us-east1.kubeconfig | cat ${DEMO_ROOT}/clusters/us-east1.yaml - > ${DEMO_ROOT}/clusters/kind/us-east1.yaml
+clusters="$@"
+if [[ $# -eq 0 ]]; then
+  clusters="us-east1 us-west1"
+fi
 
-kind create cluster --config ${DEMO_ROOT}/clusters/kind/us-west1.config --kubeconfig ${DEMO_ROOT}/clusters/kind/us-west1.kubeconfig
-sed -e 's/^/    /' ${DEMO_ROOT}/clusters/kind/us-west1.kubeconfig | cat ${DEMO_ROOT}/clusters/us-west1.yaml - > ${DEMO_ROOT}/clusters/kind/us-west1.yaml
+for name in ${clusters}; do
+  if [[ ! -f ${DEMO_ROOT}/clusters/kind/${name}.config ]]; then
+    cat > ${DEMO_ROOT}/clusters/kind/${name}.config << EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: ${name}
+networking:
+  apiServerAddress: "127.0.0.1"
+EOF
+  fi
+
+  kind delete cluster --name=${name} || true
+  kind create cluster --config ${DEMO_ROOT}/clusters/kind/${name}.config --kubeconfig ${DEMO_ROOT}/clusters/kind/${name}.kubeconfig
+
+  cat > ${DEMO_ROOT}/clusters/${name}.yaml << EOF
+apiVersion: cluster.example.dev/v1alpha1
+kind: Cluster
+metadata:
+  name: ${name}
+spec:
+  kubeconfig: |
+EOF
+  sed -e 's/^/    /' ${DEMO_ROOT}/clusters/kind/${name}.kubeconfig >> ${DEMO_ROOT}/clusters/${name}.yaml
+done
