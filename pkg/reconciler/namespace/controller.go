8786e80620db9cf5c5c799f5328a1b598c253a72
Jason Hall <jasonhall@redhat.com>
2021-09-27 10:17:27 -0400

    Schedule namespaces and all resources in them to clusters
    This does not yet include:
    - syncer transforming namespace names when applying to physical clusters

---
+++ pkg/reconciler/namespace/controller.go
@@ -0,0 +1,415 @@
+/*
+Copyright 2021 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package namespace
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+	coreinformers "k8s.io/client-go/informers/core/v1"
+	coreclient "k8s.io/client-go/kubernetes/typed/core/v1"
+	corelisters "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clusters"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+
+	clusterinformer "github.com/kcp-dev/kcp/pkg/client/informers/externalversions/cluster/v1alpha1"
+	clusterlisters "github.com/kcp-dev/kcp/pkg/client/listers/cluster/v1alpha1"
+	"github.com/kcp-dev/kcp/pkg/gvk"
+	"github.com/kcp-dev/kcp/pkg/informer"
+)
+
+const controllerName = "namespace-scheduler"
+
+// NewController returns a new Controller which schedules namespaced resources to a Cluster.
+func NewController(
+	dynClient dynamic.Interface,
+	disco *discovery.DiscoveryClient,
+	clusterInformer clusterinformer.ClusterInformer,
+	clusterLister clusterlisters.ClusterLister,
+	namespaceInformer coreinformers.NamespaceInformer,
+	namespaceLister corelisters.NamespaceLister,
+	namespaceClient coreclient.NamespaceInterface,
+	gvkTrans *gvk.GVKTranslator,
+) *Controller {
+
+	resourceQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+	gvrQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+	namespaceQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+	clusterQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	c := &Controller{
+		resourceQueue:  resourceQueue,
+		gvrQueue:       gvrQueue,
+		namespaceQueue: namespaceQueue,
+		clusterQueue:   clusterQueue,
+
+		dynClient:       dynClient,
+		clusterLister:   clusterLister,
+		namespaceLister: namespaceLister,
+		namespaceClient: namespaceClient,
+		gvkTrans:        gvkTrans,
+
+		syncChecks: []cache.InformerSynced{
+			namespaceInformer.Informer().HasSynced,
+			clusterInformer.Informer().HasSynced,
+		},
+	}
+	clusterInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) { c.enqueueCluster(obj) },
+		UpdateFunc: func(_, obj interface{}) { c.enqueueCluster(obj) },
+		DeleteFunc: func(obj interface{}) { c.enqueueCluster(obj) },
+	})
+	namespaceInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: filterNamespace,
+		Handler: cache.ResourceEventHandlerFuncs{
+			AddFunc:    func(obj interface{}) { c.enqueueNamespace(obj) },
+			UpdateFunc: func(_, obj interface{}) { c.enqueueNamespace(obj) },
+			DeleteFunc: nil, // Nothing to do.
+		},
+	})
+	c.ddsif = informer.NewDynamicDiscoverySharedInformerFactory(disco, dynClient,
+		filterResource,
+		informer.GVREventHandlerFuncs{
+			AddFunc:    func(gvr schema.GroupVersionResource, obj interface{}) { c.enqueueResource(gvr, obj) },
+			UpdateFunc: func(gvr schema.GroupVersionResource, _, obj interface{}) { c.enqueueResource(gvr, obj) },
+			DeleteFunc: nil, // Nothing to do.
+		})
+
+	return c
+}
+
+type Controller struct {
+	resourceQueue  workqueue.RateLimitingInterface
+	gvrQueue       workqueue.RateLimitingInterface
+	namespaceQueue workqueue.RateLimitingInterface
+	clusterQueue   workqueue.RateLimitingInterface
+
+	dynClient       dynamic.Interface
+	clusterLister   clusterlisters.ClusterLister
+	namespaceLister corelisters.NamespaceLister
+	namespaceClient coreclient.NamespaceInterface
+	ddsif           informer.DynamicDiscoverySharedInformerFactory
+	gvkTrans        *gvk.GVKTranslator
+
+	syncChecks []cache.InformerSynced
+}
+
+func filterResource(obj interface{}) bool {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return false
+	}
+	_, clusterAwareName, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(err)
+		return false
+	}
+	clusterName, _ := clusters.SplitClusterAwareKey(clusterAwareName)
+	if clusterName != "admin" {
+		klog.V(2).Infof("Skipping update for non-admin cluster %q", clusterName)
+		return false
+	}
+
+	current, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		klog.V(2).Infof("Object was not Unstructured: %T", obj)
+		return false
+	}
+
+	if namespaceBlocklist.Has(current.GetNamespace()) {
+		klog.V(2).Infof("Skipping syncing namespace %q", current.GetNamespace())
+		return false
+	}
+	return true
+}
+
+func filterNamespace(obj interface{}) bool {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return false
+	}
+	_, clusterAwareName, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(err)
+		return false
+	}
+	clusterName, name := clusters.SplitClusterAwareKey(clusterAwareName)
+	if clusterName != "admin" {
+		klog.Infof("Skipping update for non-admin cluster %q", clusterName)
+		return false
+	}
+	if namespaceBlocklist.Has(name) {
+		klog.V(2).Infof("Skipping syncing namespace %q", name)
+		return false
+	}
+	return true
+}
+
+func (c *Controller) enqueueResource(gvr schema.GroupVersionResource, obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	gvrstr := strings.Join([]string{gvr.Resource, gvr.Version, gvr.Group}, ".")
+	c.resourceQueue.Add(gvrstr + "::" + key)
+}
+
+func (c *Controller) enqueueGVR(gvr schema.GroupVersionResource) {
+	gvrstr := strings.Join([]string{gvr.Resource, gvr.Version, gvr.Group}, ".")
+	c.gvrQueue.Add(gvrstr)
+}
+
+func (c *Controller) enqueueNamespace(obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.namespaceQueue.Add(key)
+}
+
+func (c *Controller) enqueueCluster(obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.clusterQueue.Add(key)
+}
+
+func (c *Controller) Start(ctx context.Context, numThreads int) {
+	defer runtime.HandleCrash()
+	defer c.resourceQueue.ShutDown()
+	defer c.gvrQueue.ShutDown()
+	defer c.namespaceQueue.ShutDown()
+	defer c.clusterQueue.ShutDown()
+
+	klog.Info("Starting Namespace scheduler")
+	defer klog.Info("Shutting down Namespace scheduler")
+
+	if !cache.WaitForNamedCacheSync("namespace-scheduler", ctx.Done(), c.syncChecks...) {
+		klog.Warning("Failed to wait for caches to sync")
+		return
+	}
+
+	c.ddsif.Start(ctx)
+
+	for i := 0; i < numThreads; i++ {
+		go wait.Until(func() { c.startResourceWorker(ctx) }, time.Second, ctx.Done())
+		go wait.Until(func() { c.startGVRWorker(ctx) }, time.Second, ctx.Done())
+		go wait.Until(func() { c.startNamespaceWorker(ctx) }, time.Second, ctx.Done())
+		go wait.Until(func() { c.startClusterWorker(ctx) }, time.Second, ctx.Done())
+	}
+	<-ctx.Done()
+}
+
+func (c *Controller) startResourceWorker(ctx context.Context) {
+	for c.processNextResource(ctx) {
+	}
+}
+func (c *Controller) startGVRWorker(ctx context.Context) {
+	for c.processNextGVR(ctx) {
+	}
+}
+func (c *Controller) startNamespaceWorker(ctx context.Context) {
+	for c.processNextNamespace(ctx) {
+	}
+}
+func (c *Controller) startClusterWorker(ctx context.Context) {
+	for c.processNextCluster(ctx) {
+	}
+}
+
+func (c *Controller) processNextResource(ctx context.Context) bool {
+	// Wait until there is a new item in the working queue
+	k, quit := c.resourceQueue.Get()
+	if quit {
+		return false
+	}
+	key := k.(string)
+
+	// No matter what, tell the queue we're done with this key, to unblock
+	// other workers.
+	defer c.resourceQueue.Done(key)
+
+	if err := c.processResource(ctx, key); err != nil {
+		runtime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", controllerName, key, err))
+		c.resourceQueue.AddRateLimited(key)
+		return true
+	}
+	c.resourceQueue.Forget(key)
+	return true
+}
+
+func (c *Controller) processNextGVR(ctx context.Context) bool {
+	// Wait until there is a new item in the working queue
+	k, quit := c.gvrQueue.Get()
+	if quit {
+		return false
+	}
+	key := k.(string)
+
+	// No matter what, tell the queue we're done with this key, to unblock
+	// other workers.
+	defer c.resourceQueue.Done(key)
+
+	if err := c.processGVR(ctx, key); err != nil {
+		runtime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", controllerName, key, err))
+		c.gvrQueue.AddRateLimited(key)
+		return true
+	}
+	c.gvrQueue.Forget(key)
+	return true
+}
+
+func (c *Controller) processNextNamespace(ctx context.Context) bool {
+	// Wait until there is a new item in the working queue
+	k, quit := c.namespaceQueue.Get()
+	if quit {
+		return false
+	}
+	key := k.(string)
+
+	// No matter what, tell the queue we're done with this key, to unblock
+	// other workers.
+	defer c.namespaceQueue.Done(key)
+
+	if err := c.processNamespace(ctx, key); err != nil {
+		runtime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", controllerName, key, err))
+		c.namespaceQueue.AddRateLimited(key)
+		return true
+	}
+	c.namespaceQueue.Forget(key)
+	return true
+}
+
+func (c *Controller) processNextCluster(ctx context.Context) bool {
+	// Wait until there is a new item in the working queue
+	k, quit := c.clusterQueue.Get()
+	if quit {
+		return false
+	}
+	key := k.(string)
+
+	// No matter what, tell the queue we're done with this key, to unblock
+	// other workers.
+	defer c.clusterQueue.Done(key)
+
+	if err := c.processCluster(ctx, key); err != nil {
+		runtime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", controllerName, key, err))
+		c.clusterQueue.AddRateLimited(key)
+		return true
+	}
+	c.clusterQueue.Forget(key)
+	return true
+}
+
+// key is gvr::KEY
+func (c *Controller) processResource(ctx context.Context, key string) error {
+	parts := strings.SplitN(key, "::", 2)
+	if len(parts) != 2 {
+		klog.Errorf("Error parsing key %q; dropping", key)
+		return nil
+	}
+	gvrstr := parts[0]
+	gvr, _ := schema.ParseResourceArg(gvrstr)
+	if gvr == nil {
+		klog.Errorf("Error parsing GVR %q; dropping", gvrstr)
+		return nil
+	}
+	key = parts[1]
+
+	obj, exists, err := c.ddsif.IndexerFor(*gvr).GetByKey(key)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		klog.Infof("object %q does not exist", key)
+		return nil
+	}
+	unstr, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		klog.Errorf("object was not Unstructured, dropping: %T", obj)
+		return nil
+	}
+	unstr = unstr.DeepCopy()
+
+	// Get logical cluster name.
+	_, clusterAwareName, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		klog.Errorf("failed to split key %q, dropping: %v", key, err)
+		return nil
+	}
+	lclusterName, _ := clusters.SplitClusterAwareKey(clusterAwareName)
+	return c.reconcileResource(ctx, lclusterName, unstr, gvr)
+}
+
+func (c *Controller) processGVR(ctx context.Context, gvrstr string) error {
+	gvr, _ := schema.ParseResourceArg(gvrstr)
+	if gvr == nil {
+		klog.Errorf("Error parsing GVR %q; dropping", gvrstr)
+		return nil
+	}
+	return c.reconcileGVR(ctx, *gvr)
+}
+
+// namespaceBlocklist holds a set of namespaces that should never be synced from kcp to physical clusters.
+var namespaceBlocklist = sets.NewString("kube-system", "kube-public", "kube-node-lease")
+
+func (c *Controller) processNamespace(ctx context.Context, key string) error {
+	ns, err := c.namespaceLister.Get(key)
+	if k8serrors.IsNotFound(err) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	// Get logical cluster name.
+	_, clusterAwareName, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		klog.Errorf("failed to split key %q, dropping: %v", key, err)
+		return nil
+	}
+	lclusterName, _ := clusters.SplitClusterAwareKey(clusterAwareName)
+
+	return c.reconcileNamespace(ctx, lclusterName, ns.DeepCopy())
+}
+
+func (c *Controller) processCluster(ctx context.Context, key string) error {
+	cluster, err := c.clusterLister.Get(key)
+	if k8serrors.IsNotFound(err) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+	return c.observeCluster(ctx, cluster.DeepCopy())
+}
---
+++ test/e2e/reconciler/namespace/controller_test.go
@@ -0,0 +1,350 @@
+/*
+Copyright 2021 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package workspace
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	corev1 "k8s.io/api/core/v1"
+	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	clusterv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/cluster/v1alpha1"
+	clientset "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
+	"github.com/kcp-dev/kcp/test/e2e/framework"
+)
+
+const clusterLabel = "kcp.dev/cluster"
+
+func TestNamespaceScheduler(t *testing.T) {
+	// TODO(jasonhall): Make tests pass.
+	t.Skip("Tests currently don't pass")
+
+	var testCases = []struct {
+		name string
+		work func(ctx context.Context, t framework.TestingTInterface, kubeClient kubernetes.Interface, client clientset.Interface, watcher watch.Interface)
+	}{
+		{
+			name: "create a namespace without clusters, expect it to be unschedulable",
+			work: func(ctx context.Context, t framework.TestingTInterface, kubeClient kubernetes.Interface, client clientset.Interface, watcher watch.Interface) {
+				namespace, err := kubeClient.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "steve"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create namespace: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Added, exactMatcher(namespace), 5*time.Second); err != nil {
+					t.Errorf("did not see workspace created: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, unschedulableMatcher(), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+			},
+		},
+		{
+			name: "add a cluster after a namespace is unschedulable, expect it to be scheduled",
+			work: func(ctx context.Context, t framework.TestingTInterface, kubeClient kubernetes.Interface, client clientset.Interface, watcher watch.Interface) {
+				namespace, err := kubeClient.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "steve"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create namespace: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Added, exactMatcher(namespace), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace created: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, unschedulableMatcher(), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+				bostonCluster, err := client.ClusterV1alpha1().Clusters().Create(ctx, &clusterv1alpha1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: "boston"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create cluster: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, scheduledMatcher(bostonCluster.Name), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+			},
+		},
+		{
+			name: "create a namespace with a cluster, expect it to be scheduled",
+			work: func(ctx context.Context, t framework.TestingTInterface, kubeClient kubernetes.Interface, client clientset.Interface, watcher watch.Interface) {
+				bostonCluster, err := client.ClusterV1alpha1().Clusters().Create(ctx, &clusterv1alpha1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: "boston"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create first cluster: %v", err)
+					return
+				}
+				namespace, err := kubeClient.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "steve"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create namespace: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Added, exactMatcher(namespace), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace created: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, scheduledMatcher(bostonCluster.Name), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+			},
+		},
+		{
+			name: "delete a cluster that a namespace is scheduled to, expect it to move to another cluster",
+			work: func(ctx context.Context, t framework.TestingTInterface, kubeClient kubernetes.Interface, client clientset.Interface, watcher watch.Interface) {
+				bostonCluster, err := client.ClusterV1alpha1().Clusters().Create(ctx, &clusterv1alpha1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: "boston"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create first cluster: %v", err)
+					return
+				}
+				atlantaCluster, err := client.ClusterV1alpha1().Clusters().Create(ctx, &clusterv1alpha1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: "atlanta"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create second cluster: %v", err)
+					return
+				}
+				namespace, err := kubeClient.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "steve"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create namespace: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Added, exactMatcher(namespace), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace created: %v", err)
+					return
+				}
+				namespace, err = expectNextEvent(watcher, watch.Modified, scheduledAnywhereMatcher(), 5*time.Second)
+				if err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+				var otherCluster string
+				for _, name := range []string{bostonCluster.Name, atlantaCluster.Name} {
+					if name != namespace.Labels[clusterLabel] {
+						otherCluster = name
+						break
+					}
+				}
+				if err := client.ClusterV1alpha1().Clusters().Delete(ctx, namespace.Labels[clusterLabel], metav1.DeleteOptions{}); err != nil {
+					t.Errorf("failed to delete cluster: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, scheduledMatcher(otherCluster), 5*time.Second); err != nil {
+					t.Errorf("did not see workspace updated: %v", err)
+					return
+				}
+			},
+		},
+		{
+			name: "delete all clusters, expect namespace to be unschedulable",
+			work: func(ctx context.Context, t framework.TestingTInterface, kubeClient kubernetes.Interface, client clientset.Interface, watcher watch.Interface) {
+				bostonCluster, err := client.ClusterV1alpha1().Clusters().Create(ctx, &clusterv1alpha1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: "boston"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create first cluster: %v", err)
+					return
+				}
+				namespace, err := kubeClient.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "steve"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create namespace: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Added, exactMatcher(namespace), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace created: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, scheduledMatcher(bostonCluster.Name), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+				if err := client.ClusterV1alpha1().Clusters().Delete(ctx, bostonCluster.Name, metav1.DeleteOptions{}); err != nil {
+					t.Errorf("failed to delete cluster: %v", err)
+					return
+				}
+				if _, err := expectNextEvent(watcher, watch.Modified, unschedulableMatcher(), 5*time.Second); err != nil {
+					t.Errorf("did not see namespace updated: %v", err)
+					return
+				}
+			},
+		},
+	}
+	const serverName = "main"
+	for i := range testCases {
+		testCase := testCases[i]
+		framework.Run(t, testCase.name, func(t framework.TestingTInterface, servers map[string]framework.RunningServer) {
+			ctx := context.Background()
+			if deadline, ok := t.Deadline(); ok {
+				withDeadline, cancel := context.WithDeadline(ctx, deadline)
+				t.Cleanup(cancel)
+				ctx = withDeadline
+			}
+			if len(servers) != 1 {
+				t.Errorf("incorrect number of servers: %d", len(servers))
+				return
+			}
+			server := servers[serverName]
+			cfg, err := server.Config()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			clusterName, err := detectClusterName(cfg, ctx)
+			if err != nil {
+				t.Errorf("failed to detect cluster name: %v", err)
+				return
+			}
+			kubeClient, err := kubernetes.NewClusterForConfig(cfg)
+			if err != nil {
+				t.Errorf("failed to construct client for server: %v", err)
+				return
+			}
+			client := kubeClient.Cluster(clusterName)
+			watcher, err := client.CoreV1().Namespaces().Watch(ctx, metav1.ListOptions{})
+			if err != nil {
+				t.Errorf("failed to watch namespaces: %v", err)
+				return
+			}
+
+			clients, err := clientset.NewClusterForConfig(cfg)
+			if err != nil {
+				t.Errorf("failed to construct client for server: %v", err)
+				return
+			}
+			clusterClient := clients.Cluster(clusterName)
+
+			testCase.work(ctx, t, client, clusterClient, watcher)
+		}, framework.KcpConfig{
+			Name: "main",
+			Args: []string{"--install_cluster_controller", "--install_workspace_controller", "--install_namespace_scheduler"},
+		})
+	}
+}
+
+// TODO: we need to undo the prefixing and get normal sharding behavior in soon ... ?
+func detectClusterName(cfg *rest.Config, ctx context.Context) (string, error) {
+	crdClient, err := apiextensionsclientset.NewClusterForConfig(cfg)
+	if err != nil {
+		return "", fmt.Errorf("failed to construct client for server: %w", err)
+	}
+	crds, err := crdClient.Cluster("*").ApiextensionsV1().CustomResourceDefinitions().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return "", fmt.Errorf("failed to list crds: %w", err)
+	}
+	if len(crds.Items) == 0 {
+		return "", errors.New("found no crds, cannot detect cluster name")
+	}
+	for _, crd := range crds.Items {
+		if crd.ObjectMeta.Name == "workspaces.tenancy.kcp.dev" {
+			return crd.ObjectMeta.ClusterName, nil
+		}
+	}
+	return "", errors.New("detected no admin cluster")
+}
+
+func exactMatcher(expected *corev1.Namespace) matcher {
+	return func(object *corev1.Namespace) error {
+		if !apiequality.Semantic.DeepEqual(expected, object) {
+			return fmt.Errorf("incorrect object: %v", cmp.Diff(expected, object))
+		}
+		return nil
+	}
+}
+
+func unschedulableMatcher() matcher {
+	return func(object *corev1.Namespace) error {
+		if !isNamespaceUnschedulable(object) {
+			return fmt.Errorf("expected an unschedulable namespace, got status.conditions: %#v", object.Status.Conditions)
+		}
+		return nil
+	}
+}
+
+func scheduledMatcher(target string) matcher {
+	return func(object *corev1.Namespace) error {
+		if isNamespaceUnschedulable(object) {
+			return fmt.Errorf("expected a scheduled workspace, got status.conditions: %#v", object.Status.Conditions)
+		}
+		if object.Labels[clusterLabel] != target {
+			return fmt.Errorf("expected namespace assignment to be %q, got %q", target, object.Labels[clusterLabel])
+		}
+		return nil
+	}
+}
+
+func scheduledAnywhereMatcher() matcher {
+	return func(object *corev1.Namespace) error {
+		if isNamespaceUnschedulable(object) {
+			return fmt.Errorf("expected a scheduled workspace, got status.conditions: %#v", object.Status.Conditions)
+		}
+		return nil
+	}
+}
+
+type matcher func(object *corev1.Namespace) error
+
+func expectNextEvent(w watch.Interface, expectType watch.EventType, matcher matcher, duration time.Duration) (*corev1.Namespace, error) {
+	event, err := nextEvent(w, duration)
+	if err != nil {
+		return nil, err
+	}
+	if expectType != event.Type {
+		return nil, fmt.Errorf("got incorrect watch event type: %v != %v\n", expectType, event.Type)
+	}
+	workspace, ok := event.Object.(*corev1.Namespace)
+	if !ok {
+		return nil, fmt.Errorf("got %T, not a Workspace", event.Object)
+	}
+	if err := matcher(workspace); err != nil {
+		return workspace, err
+	}
+	return workspace, nil
+}
+
+func nextEvent(w watch.Interface, duration time.Duration) (watch.Event, error) {
+	stopTimer := time.NewTimer(duration)
+	defer stopTimer.Stop()
+	select {
+	case event, ok := <-w.ResultChan():
+		if !ok {
+			return watch.Event{}, errors.New("watch closed unexpectedly")
+		}
+		return event, nil
+	case <-stopTimer.C:
+		return watch.Event{}, errors.New("timed out waiting for event")
+	}
+}
+
+const conditionTypeUnschedulable = "Unschedulable"
+
+func isNamespaceUnschedulable(ns *corev1.Namespace) bool {
+	for _, cond := range ns.Status.Conditions {
+		if cond.Type == conditionTypeUnschedulable {
+			return cond.Status == corev1.ConditionTrue
+		}
+	}
+	return false
+}
