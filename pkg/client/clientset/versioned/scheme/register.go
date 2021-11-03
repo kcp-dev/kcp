c7d30e09829fbf64d83ae29206768a16e3f16b64
David Festal <dfestal@redhat.com>
2021-11-03 17:31:57 +0100

    organize imports after code generation
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/client/clientset/versioned/scheme/register.go
+++ pkg/client/clientset/versioned/scheme/register.go
@@ -19,14 +19,15 @@ limitations under the License.
 package scheme
 
 import (
-	apiresourcev1alpha1 "github.com/kcp-dev/kcp/pkg/apis/apiresource/v1alpha1"
-	clusterv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/cluster/v1alpha1"
-	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+
+	apiresourcev1alpha1 "github.com/kcp-dev/kcp/pkg/apis/apiresource/v1alpha1"
+	clusterv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/cluster/v1alpha1"
+	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
 )
 
 var Scheme = runtime.NewScheme()
--- pkg/client/informers/externalversions/tenancy/v1alpha1/workspaceshard.go
+++ pkg/client/informers/externalversions/tenancy/v1alpha1/workspaceshard.go
@@ -22,14 +22,15 @@ import (
 	"context"
 	time "time"
 
-	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
-	versioned "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
-	internalinterfaces "github.com/kcp-dev/kcp/pkg/client/informers/externalversions/internalinterfaces"
-	v1alpha1 "github.com/kcp-dev/kcp/pkg/client/listers/tenancy/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	watch "k8s.io/apimachinery/pkg/watch"
 	cache "k8s.io/client-go/tools/cache"
+
+	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
+	versioned "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
+	internalinterfaces "github.com/kcp-dev/kcp/pkg/client/informers/externalversions/internalinterfaces"
+	v1alpha1 "github.com/kcp-dev/kcp/pkg/client/listers/tenancy/v1alpha1"
 )
 
 // WorkspaceShardInformer provides access to a shared informer and lister for
