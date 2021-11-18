bd8bd56a30e37e48acabf29bd67f53ff1af197de
Steve Kuznetsov <skuznets@redhat.com>
2021-11-18 08:43:46 -0800

    localdev: revert to generic replacement command
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- contrib/localdev/localdev.sh
+++ contrib/localdev/localdev.sh
@@ -15,4 +15,4 @@ set -o pipefail
 
 OS_ROOT="$(dirname "${BASH_SOURCE}")/../.."
 
-sed -i -e 's:=>  *github\.com/kcp-dev/\(kubernetes[^ ]*\) ..*:=> /home/stevekuznetsov/code/kcp-dev/kubernetes/src/github.com/kcp-dev/\1:' "${OS_ROOT}/go.mod"
+sed -i -e 's:=>  *github\.com/kcp-dev/\(kubernetes[^ ]*\) ..*:=> ../../../k8s.io/\1:' "${OS_ROOT}/go.mod"
