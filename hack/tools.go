0fa88d682c74e233b3253f7ba50fb8beffb9cab4
Andy Goldstein <andy.goldstein@redhat.com>
2021-11-19 13:32:53 -0500

    Pin controller-gen to v0.7.0

--- hack/tools.go
+++ hack/tools.go
@@ -22,6 +22,4 @@ package tools
 // This package imports things required by this repository, to force `go mod` to see them as dependencies
 import (
 	_ "github.com/coreydaley/openshift-goimports"
-
-	_ "k8s.io/code-generator"
 )
