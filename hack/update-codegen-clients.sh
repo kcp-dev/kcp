ba42888f2d1a03aca8628c42ab75e41bd4f8c16b
Steve Kuznetsov <skuznets@redhat.com>
2021-12-01 09:45:53 -0800

    ci: verify our boilerplate
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/update-codegen-clients.sh
+++ hack/update-codegen-clients.sh
@@ -13,4 +13,4 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; go list -f '{{.Dir}}' -m k8s.i
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/kcp-dev/kcp/pkg/client github.com/kcp-dev/kcp/pkg/apis \
   "cluster:v1alpha1 apiresource:v1alpha1 tenancy:v1alpha1" \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt --output-base ${GOPATH}/src
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.go.txt --output-base ${GOPATH}/src
