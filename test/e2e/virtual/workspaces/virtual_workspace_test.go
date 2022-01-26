9e8e895abdede0b3be4c101c555c251b573f683f
David Festal <dfestal@redhat.com>
2022-01-26 13:34:53 +0100

    `workspaces/kubeconfig` subresource E2E test
    Signed-off-by: David Festal <dfestal@redhat.com>

--- test/e2e/virtual/workspaces/virtual_workspace_test.go
+++ test/e2e/virtual/workspaces/virtual_workspace_test.go
@@ -24,10 +24,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	v1 "k8s.io/api/core/v1"
 	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 
 	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
 	clientset "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
@@ -35,6 +39,7 @@ import (
 	workspacescmd "github.com/kcp-dev/kcp/pkg/virtual/workspaces/cmd"
 	"github.com/kcp-dev/kcp/test/e2e/framework"
 	"github.com/kcp-dev/kcp/test/e2e/virtual/helpers"
+	"github.com/kcp-dev/kcp/third_party/conditions/util/conditions"
 )
 
 type testDataType struct {
@@ -70,6 +75,7 @@ var testData = testDataType{
 func TestWorkspacesVirtualWorkspaces(t *testing.T) {
 	type runningServer struct {
 		framework.RunningServer
+		kubeClient                     kubernetes.Interface
 		kcpClient                      clientset.Interface
 		kcpExpect                      framework.RegisterWorkspaceExpectation
 		virtualWorkspaceClientContexts []helpers.VirtualWorkspaceClientContext
@@ -146,6 +152,136 @@ func TestWorkspacesVirtualWorkspaces(t *testing.T) {
 				}
 			},
 		},
+		{
+			name: "create a workspace in personal virtual workspace and retrieve its kubeconfig",
+			virtualWorkspaceClientContexts: []helpers.VirtualWorkspaceClientContext{
+				{
+					User:   testData.user1,
+					Prefix: "/personal",
+				},
+			},
+			work: func(ctx context.Context, t framework.TestingTInterface, server runningServer) {
+				vwUser1Client := server.virtualWorkspaceClients[0]
+				_, err := server.kubeClient.CoreV1().Namespaces().Create(ctx, &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "default"}}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create namespace: %v", err)
+					return
+				}
+				kcpServerKubeconfig, err := server.RunningServer.RawConfig()
+				if err != nil {
+					t.Errorf("failed to get KCP Kubeconfig: %v", err)
+					return
+				}
+				kubeconfigContent, err := clientcmd.Write(kcpServerKubeconfig)
+				if err != nil {
+					t.Errorf("failed to get KCP Kubeconfig content: %v", err)
+					return
+				}
+				_, err = server.kubeClient.CoreV1().Secrets("default").Create(ctx, &v1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "kubeconfig",
+						Namespace: "default",
+					},
+					StringData: map[string]string{
+						"kubeconfig": string(kubeconfigContent),
+					},
+				}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create workspace shard secret: %v", err)
+					return
+				}
+				_, err = server.kcpClient.TenancyV1alpha1().WorkspaceShards().Create(ctx, &tenancyv1alpha1.WorkspaceShard{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "boston",
+					},
+					Spec: tenancyv1alpha1.WorkspaceShardSpec{
+						Credentials: v1.SecretReference{
+							Name:      "kubeconfig",
+							Namespace: "default",
+						},
+					},
+				}, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create workspace shard: %v", err)
+					return
+				}
+				defer server.Artifact(t, func() (runtime.Object, error) {
+					return server.kcpClient.TenancyV1alpha1().WorkspaceShards().Get(ctx, "boston", metav1.GetOptions{})
+				})
+				workspace1, err := vwUser1Client.TenancyV1alpha1().Workspaces().Create(ctx, testData.workspace1, metav1.CreateOptions{})
+				if err != nil {
+					t.Errorf("failed to create workspace: %v", err)
+					return
+				}
+				assert.Equal(t, testData.workspace1.Name, workspace1.Name)
+				defer server.Artifact(t, func() (runtime.Object, error) {
+					return server.kcpClient.TenancyV1alpha1().Workspaces().Get(ctx, testData.workspace1.Name, metav1.GetOptions{})
+				})
+				workspaceURL := ""
+				if err := server.kcpExpect(workspace1, func(w *tenancyv1alpha1.Workspace) error {
+					if !conditions.IsTrue(w, tenancyv1alpha1.WorkspaceURLValid) {
+						return fmt.Errorf("Workspace %s is not valid: %s", w.Name, conditions.GetMessage(w, tenancyv1alpha1.WorkspaceURLValid))
+					}
+					workspaceURL = w.Status.BaseURL
+					return nil
+				}); err != nil {
+					t.Errorf("did not see the workspace created and valid in KCP: %v", err)
+					return
+				}
+
+				if err := server.virtualWorkspaceExpectations[0](func(w *tenancyv1alpha1.WorkspaceList) error {
+					if len(w.Items) != 1 || w.Items[0].Name != workspace1.Name {
+						return fmt.Errorf("expected only one workspace (%s), got %#v", workspace1.Name, w)
+					}
+					return nil
+				}); err != nil {
+					t.Errorf("did not see the workspace created in personal virtual workspace: %v", err)
+					return
+				}
+
+				req := vwUser1Client.TenancyV1alpha1().RESTClient().Get().Resource("workspaces").Name(workspace1.Name).SubResource("kubeconfig").Do(ctx)
+				if req.Error() != nil {
+					t.Errorf("error retrieving the kubeconfig for workspace %s: %v", workspace1.Name, err)
+				}
+				kcpConfigCurrentContextName := kcpServerKubeconfig.CurrentContext
+				kcpConfigCurrentContext := kcpServerKubeconfig.Contexts[kcpConfigCurrentContextName]
+				if kcpConfigCurrentContext == nil {
+					t.Errorf("kcp Kubeconfig is invalid:\n %+v", kcpServerKubeconfig)
+					return
+				}
+				kcpConfigCurrentCluster := kcpServerKubeconfig.Clusters[kcpConfigCurrentContext.Cluster]
+				if kcpConfigCurrentCluster == nil {
+					t.Errorf("kcp Kubeconfig is invalid:\n %+v", kcpServerKubeconfig)
+					return
+				}
+
+				expectedKubeconfigCluster := kcpConfigCurrentCluster.DeepCopy()
+				expectedKubeconfigCluster.Server = workspaceURL
+				expectedKubeconfig := &api.Config{
+					CurrentContext: "personal/" + workspace1.Name,
+					Contexts: map[string]*api.Context{
+						"personal/" + workspace1.Name: {
+							Cluster: "personal/" + workspace1.Name,
+						},
+					},
+					Clusters: map[string]*api.Cluster{
+						"personal/" + workspace1.Name: expectedKubeconfigCluster,
+					},
+				}
+				expectedKubeconfigContent, err := clientcmd.Write(*expectedKubeconfig)
+				if err != nil {
+					t.Errorf("error writing the content of the expected kubeconfig for workspace %s: %v", workspace1.Name, err)
+					return
+				}
+				workspaceKubeconfigContent, err := req.Raw()
+				if err != nil {
+					t.Errorf("error retrieving the content of the kubeconfig for workspace %s: %v", workspace1.Name, err)
+					return
+				}
+
+				assert.YAMLEq(t, string(expectedKubeconfigContent), string(workspaceKubeconfigContent))
+			},
+		},
 	}
 
 	const serverName = "main"
@@ -220,6 +356,12 @@ func TestWorkspacesVirtualWorkspaces(t *testing.T) {
 				t.Errorf("failed to detect cluster name: %v", err)
 				return
 			}
+			kubeClients, err := kubernetes.NewClusterForConfig(kcpCfg)
+			if err != nil {
+				t.Errorf("failed to construct client for server: %v", err)
+				return
+			}
+			kubeClient := kubeClients.Cluster(clusterName)
 			kcpClients, err := clientset.NewClusterForConfig(kcpCfg)
 			if err != nil {
 				t.Errorf("failed to construct client for server: %v", err)
@@ -234,6 +376,7 @@ func TestWorkspacesVirtualWorkspaces(t *testing.T) {
 
 			testCase.work(ctx, t, runningServer{
 				RunningServer:                  server,
+				kubeClient:                     kubeClient,
 				kcpClient:                      kcpClient,
 				kcpExpect:                      kcpExpect,
 				virtualWorkspaceClientContexts: testCase.virtualWorkspaceClientContexts,
