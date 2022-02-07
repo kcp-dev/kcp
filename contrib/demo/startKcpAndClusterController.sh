8bafb53ce1cc74575498f37e3ac4c74e5b822b00
Andy Goldstein <andy.goldstein@redhat.com>
2022-02-07 13:56:35 -0500

    contrib/demo: shellcheck
    Signed-off-by: Andy Goldstein <andy.goldstein@redhat.com>

--- contrib/demo/startKcpAndClusterController.sh
+++ contrib/demo/startKcpAndClusterController.sh
@@ -15,24 +15,23 @@
 # limitations under the License.
 
 CURRENT_DIR="$(pwd)"
-DEMOS_DIR="$(dirname "${BASH_SOURCE}")"
-KCP_DIR="$(cd ${DEMOS_DIR}/../.. && pwd)"
+DEMOS_DIR="$(dirname "${BASH_SOURCE[0]}")"
+KCP_DIR="$(cd "${DEMOS_DIR}"/../.. && pwd)"
 KCP_DATA_DIR=${KCP_DATA_DIR:-$KCP_DIR}
-
 
 KUBECONFIG=${KCP_DATA_DIR}/.kcp/admin.kubeconfig
 
-source ${DEMOS_DIR}/.startUtils
-setupTraps $0 "rm -Rf ${CURRENT_DIR}/.kcp.running"
+source "${DEMOS_DIR}"/.startUtils
+setupTraps "$0" "rm -Rf ${CURRENT_DIR}/.kcp.running"
 
 KCP_FLAGS=${KCP_FLAGS:-""}
 
-if [ ! -z "${KCP_LISTEN_ADDR}" ]; then
+if [ -n "${KCP_LISTEN_ADDR}" ]; then
     KCP_FLAGS="--listen=${KCP_LISTEN_ADDR} ${KCP_FLAGS}"
 fi
 
 echo "Starting KCP server ..."
-(cd ${KCP_DATA_DIR} && exec ${KCP_DIR}/bin/kcp start ${KCP_FLAGS}) &> kcp.log &
+(cd "${KCP_DATA_DIR}" && exec "${KCP_DIR}"/bin/kcp start "${KCP_FLAGS}") &> kcp.log &
 KCP_PID=$!
 echo "KCP server started: $KCP_PID"
 
@@ -40,11 +39,11 @@ echo "Waiting for KCP server to be up and running..."
 wait_command "grep 'Serving securely' ${CURRENT_DIR}/kcp.log"
 
 echo "Applying CRDs..."
-kubectl --kubeconfig $KUBECONFIG apply -f config/
+kubectl --kubeconfig "$KUBECONFIG" apply -f config/
 
 echo ""
 echo "Starting Cluster Controller..."
-${KCP_DIR}/bin/cluster-controller --push-mode=true --pull-mode=false --kubeconfig=${KUBECONFIG} "$@" &> cluster-controller.log &
+"${KCP_DIR}"/bin/cluster-controller --push-mode=true --pull-mode=false --kubeconfig="${KUBECONFIG}" "$@" &> cluster-controller.log &
 CC_PID=$!
 echo "Cluster Controller started: $CC_PID"
 
