da59869fbdf26d398d5670f94e789e80eb3341ab
Steve Kuznetsov <skuznets@redhat.com>
2022-02-08 15:39:25 -0800

    boilerplate: fix headers to have a year
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- pkg/client/clientset/versioned/typed/cluster/v1alpha1/cluster_client.go
+++ pkg/client/clientset/versioned/typed/cluster/v1alpha1/cluster_client.go
@@ -1,5 +1,5 @@
 /*
-Copyright The KCP Authors.
+Copyright 2022 The KCP Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
