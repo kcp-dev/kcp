3cb8c5aa4850166793a5087b1f3abef0b933cc65
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-02-17 21:59:04 +0100

    authorization: rename org to bootstrap authorizer

--- pkg/authorization/bootstrap_policy_authorizer.go
+++ pkg/authorization/bootstrap_policy_authorizer.go
@@ -19,13 +19,14 @@ package authorization
 import (
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	clientgoinformers "k8s.io/client-go/informers"
+	"k8s.io/kubernetes/pkg/genericcontrolplane"
 	"k8s.io/kubernetes/plugin/pkg/auth/authorizer/rbac"
 
 	frameworkrbac "github.com/kcp-dev/kcp/pkg/virtual/framework/rbac"
 )
 
-func NewOrgWorkspaceAuthorizer(informers clientgoinformers.SharedInformerFactory) (authorizer.Authorizer, authorizer.RuleResolver) {
-	filteredInformer := frameworkrbac.FilterPerCluster("admin", informers.Rbac().V1())
+func NewBootstrapPolicyAuthorizer(informers clientgoinformers.SharedInformerFactory) (authorizer.Authorizer, authorizer.RuleResolver) {
+	filteredInformer := frameworkrbac.FilterPerCluster(genericcontrolplane.RootClusterName, informers.Rbac().V1())
 
 	a := rbac.New(
 		&rbac.RoleGetter{Lister: filteredInformer.Roles().Lister()},
