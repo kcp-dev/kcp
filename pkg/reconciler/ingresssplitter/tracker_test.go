30b77634d264bf4f7450b15532f8b7be22f2684f
Joaquim Moreno <joaquim@redhat.com>
2022-02-23 12:05:22 +0000

    reconciler/ingresssplitter/tracker_test.go: openshift-goimports

--- pkg/reconciler/ingresssplitter/tracker_test.go
+++ pkg/reconciler/ingresssplitter/tracker_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
