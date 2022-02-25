786fb5f04b130738078ecef6e21dd9fa59274da3
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-02-25 17:30:15 +0100

    e2e/namespace: import fix

--- test/e2e/reconciler/namespace/controller_test.go
+++ test/e2e/reconciler/namespace/controller_test.go
@@ -26,8 +26,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
