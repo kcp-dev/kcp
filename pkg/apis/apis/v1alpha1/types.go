e42d2ca31ca3bcf2d1f407c0f3bc0713927d07c0
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-03-02 14:36:03 +0100

    apis: extend types for exporting and binding APIs cross-workspace
    - schema evolution
    - platform global export support
    - conversion

--- pkg/apis/apis/v1alpha1/types.go
+++ pkg/apis/apis/v1alpha1/types.go
@@ -21,6 +21,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
+	"github.com/kcp-dev/kcp/pkg/apis/exports/v1alpha1"
 	conditionsv1alpha1 "github.com/kcp-dev/kcp/third_party/conditions/apis/conditions/v1alpha1"
 )
 
@@ -60,6 +61,12 @@ type APIBindingSpec struct {
 // ExportReference describes a reference to an APIExport. Exactly one of the
 // fields must be set.
 type ExportReference struct {
+	// export is a reference to an exports.kcp.dev/v1alpha1 Export object uniquely
+	// identifying the APIExport to bind to.
+	//
+	// +optional
+	Export *v1alpha1.ExportBindingReference `json:"export,omitempty"`
+
 	// workspace is a reference to an APIExport in the same organization. The creator
 	// of the APIBinding needs to have access to the APIExport with the verb `bind`
 	// in order to bind to it.
@@ -309,6 +316,45 @@ type APIResourceSchemaSpec struct {
 	// +listMapKey=name
 	// +kubebuilder:validation:MinItems=1
 	Versions []APIResourceVersion `json:"versions"`
+
+	// predecessors are references to APIResourceSchemas this one is an evolution of.
+	// Predecessors are required when
+	// 1. an APIExport spec sets another schema as latest schema available. The new
+	//    schemas must be (transitively) evolutions of the previous schemas
+	//    (if the old schema is used in at least one APIBinding in another workspace).
+	// 2. an APIBinding is bound to a new schema. The new schema must be (transitively)
+	//    an evolution of the previously bound schema.
+	//
+	// Note: (2) follows from (1) if APIBindings can only be bound to the latest schema
+	//       of APIExports.
+	//
+	// A schema is an evolution of another schema if
+	// - it has the same group, resource, scopes, names, and
+	// - it serves the same versions, and
+	// - for every version:
+	//   - the status subresource is either served in both or in none, and
+	//   - it has not less non-status subresources than the predecessor, and
+	//   - it has an OpenAPI v3 schema that is a compatible evolution of the
+	//     predecessor schema.
+	//
+	// These rules are meant to protect the API author from accidentally introducing
+	// breaking API changes. Overrides can be set for some rules by the API author
+	// to opt-out of the checks, as a conscious decision during API design.
+	//
+	// +optional
+	// +listType=map
+	// +listMapKey=name
+	Predecessors []PredecessorSchema `json:"predecessors,omitempty"`
+
+	// Conversions are conversions from one version to the new versions. The
+	// map must be exhaustive, i.e. it must contain a mapping for every possible
+	// version to every possible version.
+	//
+	// +optional
+	// +listType=map
+	// +listMapKey=from
+	// +listMapKey=to
+	Conversions []APIResourceVersionConversion `json:"conversions,omitempty"`
 }
 
 // APIResourceVersion describes one API version of a resource.
@@ -362,6 +408,110 @@ type APIResourceVersion struct {
 	// +listType=map
 	// +listMapKey=name
 	AdditionalPrinterColumns []apiextensionsv1.CustomResourceColumnDefinition `json:"additionalPrinterColumns,omitempty"`
+}
+
+// PredecessorSchema references a APIResourceSchema that this schema is an evolution of.
+type PredecessorSchema struct {
+	// name is the name of another APIResourceSchema in the same workspace.
+	//
+	// +required
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name"`
+
+	// overrides are acknowledgement by the API author that the formally breaking
+	// API changes are acceptable.
+	//
+	// WARNING: overriding the API breakage checks can lead to objects that cannot
+	// be updated anymore, or to clients that do not work anymore after the schema
+	// change.
+	//
+	// +optional
+	Overrides *PredecessorOverrides `json:"overrides,omitempty"`
+	// versionOverrides are acknowledgements by the API author that the formally breaking
+	// API changes of the given versions are acceptable.
+	//
+	// WARNING: overriding the API breakage checks can lead to objects that cannot
+	// be updated anymore, or to clients that do not work anymore after the schema
+	// change.
+	//
+	// +optional
+	// +listType=map
+	// +listMapKey=name
+	VersionOverrides []PredecessorPerVersionOverrides `json:"versionOverrides,omitempty"`
+}
+
+// PredecessorOverrides is a list of acknowledgments by the API author of
+// formally breaking API changes.
+type PredecessorOverrides struct {
+}
+
+// PredecessorPerVersionOverrides is a list of acknowledgments by the API author of
+// formally breaking API changes of given given version.
+type PredecessorPerVersionOverrides struct {
+	// name is the version name, e.g. “v1”, “v2beta1”, etc.
+	// +required
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name"`
+
+	// overrideAddedStatusSubresource acknowledges that the predecessor schema has
+	// no status subresource, but the new one has. This is a breaking API change.
+	//
+	// +optional
+	AddedStatusSubresource bool `json:"addedStatusSubresource,omitempty"`
+	// overrideRemovedStatusSubresource acknowledges that the predecessor schema has
+	// a status subresource, but the new one does not. This is a breaking API change.
+	//
+	// +optional
+	RemovedStatusSubresource bool `json:"removedStatusSubresource,omitempty"`
+	// overrideIncompatibleSchema acknowledges that the predecessor schema is incompatible
+	// with the new one. This is a breaking API change.
+	//
+	// It might be acceptable to do these kind of schema changes if additional
+	// considerations show no or low risk that objects exists in workspaces that
+	// are not compatible with the new schema, but have been with the old.
+	//
+	// +optional
+	IncompatibleSchema bool `json:"incompatibleSchema,omitempty"`
+
+	// notServedAnymore acknowledges that the predecessor schema served this
+	// API version, but the new one does not. The old HTTP API will not work
+	// anymore and client might fail.
+	//
+	// +optional
+	NotServedAnymore bool `json:"notServedAnymore,omitempty"`
+
+	// ...
+}
+
+// APIResourceVersionConversion is a conversion from one version to another version.
+type APIResourceVersionConversion struct {
+	// from is the source version for the conversion.
+	//
+	// +required
+	// +kubebuilder:validation:MinLength=1
+	From string `json:"from"`
+
+	// from is the source version for the conversion.
+	//
+	// +required
+	// +kubebuilder:validation:MinLength=1
+	To string `json:"to"`
+
+	// CEL defines a CEL based conversion function.
+	//
+	// +optional
+	CEL *CELConversion `json:"CEL,omitempty"`
+}
+
+// CELConversion specifies a conversion mechanism using CEL.
+type CELConversion struct {
+	// expression is the CEL expression to be used. The old object is accessible
+	// through the variable “old”. The new object is accessible through the variable
+	// "self".
+	//
+	// +required
+	// +kubebuilder:validation:MinLength=1
+	Expression string `json:"expression"`
 }
 
 // APIResourceSchemaList is a list of APIResourceSchema resources
