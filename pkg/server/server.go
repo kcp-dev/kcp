e3f225a3dbd9de9b07674aaf63a1b08e8fc74609
Joaquim Moreno <joaquim@redhat.com>
2022-03-09 14:49:16 +0000

    pkg/server/server.go: starts by default the RootCAConfigMapController with the kcp server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -356,6 +356,10 @@ func (s *Server) Run(ctx context.Context) error {
 		return err
 	}
 
+	if err := s.installRootCAConfigMapController(ctx, serverChain.GenericControlPlane.GenericAPIServer.LoopbackClientConfig); err != nil {
+		return err
+	}
+
 	enabled := sets.NewString(s.options.Controllers.IndividuallyEnabled...)
 	if len(enabled) > 0 {
 		klog.Infof("Starting controllers individually: %v", enabled)
