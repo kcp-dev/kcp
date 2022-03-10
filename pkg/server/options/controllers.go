feefa335a18db7a48fb4dfb16d29961fcfe6d9cf
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-03-10 16:26:13 +0100

    server: fix SAController defaults

--- pkg/server/options/controllers.go
+++ pkg/server/options/controllers.go
@@ -19,7 +19,8 @@ package options
 import (
 	"github.com/spf13/pflag"
 
-	saconfig "k8s.io/kubernetes/pkg/controller/serviceaccount/config"
+	"k8s.io/klog/v2"
+	kcmoptions "k8s.io/kubernetes/cmd/kube-controller-manager/app/options"
 
 	"github.com/kcp-dev/kcp/pkg/reconciler/apiresource"
 	"github.com/kcp-dev/kcp/pkg/reconciler/cluster/apiimporter"
@@ -32,20 +33,32 @@ type Controllers struct {
 	ApiImporter         ApiImporterController
 	ApiResource         ApiResourceController
 	Syncer              SyncerController
-	SAController        saconfig.SAControllerConfiguration
+	SAController        kcmoptions.SAControllerOptions
 }
 
 type ApiImporterController = apiimporter.Options
 type ApiResourceController = apiresource.Options
 type SyncerController = syncer.Options
 
+var kcmDefaults *kcmoptions.KubeControllerManagerOptions
+
+func init() {
+	var err error
+
+	kcmDefaults, err = kcmoptions.NewKubeControllerManagerOptions()
+	if err != nil {
+		klog.Fatal(err)
+	}
+}
+
 func NewControllers() *Controllers {
 	return &Controllers{
 		EnableAll: true,
 
-		ApiImporter: *apiimporter.DefaultOptions(),
-		ApiResource: *apiresource.DefaultOptions(),
-		Syncer:      *syncer.DefaultOptions(),
+		ApiImporter:  *apiimporter.DefaultOptions(),
+		ApiResource:  *apiresource.DefaultOptions(),
+		Syncer:       *syncer.DefaultOptions(),
+		SAController: *kcmDefaults.SAController,
 	}
 }
 
@@ -55,13 +68,11 @@ func (c *Controllers) AddFlags(fs *pflag.FlagSet) {
 	fs.StringSliceVar(&c.IndividuallyEnabled, "unsupported-run-individual-controllers", c.IndividuallyEnabled, "Run individual controllers in-process. The controller names can change at any time.")
 	fs.MarkHidden("unsupported-run-individual-controllers") //nolint:errcheck
 
-	fs.StringVar(&c.SAController.ServiceAccountKeyFile, "service-account-private-key-file", c.SAController.ServiceAccountKeyFile, "Filename containing a PEM-encoded private RSA or ECDSA key used to sign service account tokens.")
-	fs.Int32Var(&c.SAController.ConcurrentSATokenSyncs, "concurrent-serviceaccount-token-syncs", c.SAController.ConcurrentSATokenSyncs, "The number of service account token objects that are allowed to sync concurrently. Larger number = more responsive token generation, but more CPU (and network) load")
-	fs.StringVar(&c.SAController.RootCAFile, "root-ca-file", c.SAController.RootCAFile, "If set, this root certificate authority will be included in service account's token secret. This must be a valid PEM-encoded CA bundle.")
-
 	apiimporter.BindOptions(&c.ApiImporter, fs)
 	apiresource.BindOptions(&c.ApiResource, fs)
 	syncer.BindOptions(&c.Syncer, fs)
+
+	c.SAController.AddFlags(fs)
 }
 
 func (c *Controllers) Validate() []error {
@@ -75,6 +86,9 @@ func (c *Controllers) Validate() []error {
 	}
 	if err := c.Syncer.Validate(); err != nil {
 		errs = append(errs, err)
+	}
+	if saErrs := c.SAController.Validate(); saErrs != nil {
+		errs = append(errs, saErrs...)
 	}
 
 	return errs
