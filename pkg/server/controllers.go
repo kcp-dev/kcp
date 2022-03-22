d4f5316b9c44a720b4aed12abc2f65f485293cde
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-03-22 14:35:49 +0100

    server: fix lint error in installApiResourceController

--- pkg/server/controllers.go
+++ pkg/server/controllers.go
@@ -449,6 +449,9 @@ func (s *Server) installApiResourceController(ctx context.Context, config *rest.
 		s.kcpSharedInformerFactory.Apiresource().V1alpha1().APIResourceImports(),
 		s.apiextensionsSharedInformerFactory.Apiextensions().V1().CustomResourceDefinitions(),
 	)
+	if err != nil {
+		return err
+	}
 
 	s.AddPostStartHook("kcp-install-api-resource-controller", func(hookContext genericapiserver.PostStartHookContext) error {
 		// HACK HACK HACK
