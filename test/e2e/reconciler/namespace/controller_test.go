dd4e336f64700216e6c3c80fd30eeb119fbf5285
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-05-05 10:19:31 +0200

    e2e/reconciler/namespace: add another test with CRDs coming and going

--- test/e2e/reconciler/namespace/controller_test.go
+++ test/e2e/reconciler/namespace/controller_test.go
@@ -23,17 +23,28 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/kcp-dev/apimachinery/pkg/logicalcluster"
 	"github.com/stretchr/testify/require"
 
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
 
+	configcrds "github.com/kcp-dev/kcp/config/crds"
+	workloadv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/workload/v1alpha1"
 	"github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
 	clientset "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
 	nscontroller "github.com/kcp-dev/kcp/pkg/reconciler/workload/namespace"
@@ -64,7 +75,6 @@ func TestNamespaceScheduler(t *testing.T) {
 			name: "validate namespace scheduling",
 			work: func(ctx context.Context, t *testing.T, server runningServer) {
 				t.Log("Create a namespace without a cluster available and expect it to be marked unschedulable")
-
 				namespace, err := server.client.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{
 					ObjectMeta: metav1.ObjectMeta{
 						GenerateName: "e2e-nss-",
@@ -124,6 +134,100 @@ func TestNamespaceScheduler(t *testing.T) {
 
 				err = server.expect(namespace, unscheduledMatcher(nscontroller.NamespaceReasonSchedulingDisabled))
 				require.NoError(t, err, "did not see namespace marked with scheduling disabled")
+			},
+		},
+		{
+			name: "new GVRs are scheduled",
+			work: func(ctx context.Context, t *testing.T, server runningServer) {
+				crdClusterClient, err := apiextensionsclient.NewClusterForConfig(server.DefaultConfig(t))
+				require.NoError(t, err, "failed to construct apiextensions client for server")
+				dynamicClusterClient, err := dynamic.NewClusterForConfig(server.DefaultConfig(t))
+				require.NoError(t, err, "failed to construct dynamic client for server")
+				kubeClusterClient, err := kubernetes.NewClusterForConfig(server.DefaultConfig(t))
+				require.NoError(t, err, "failed to construct kubernetes client for server")
+
+				t.Log("Create a ready WorkloadCluster, and keep it artificially ready") // we don't want the syncer to do anything with CRDs, hence we fake the syncer
+				cluster := &workloadv1alpha1.WorkloadCluster{
+					ObjectMeta: metav1.ObjectMeta{Name: "cluster7"},
+					Spec:       workloadv1alpha1.WorkloadClusterSpec{},
+				}
+				cluster, err = server.kcpClient.WorkloadV1alpha1().WorkloadClusters().Create(ctx, cluster, metav1.CreateOptions{})
+				require.NoError(t, err, "failed to create cluster")
+				require.Eventually(t, func() bool {
+					cluster, err = server.kcpClient.WorkloadV1alpha1().WorkloadClusters().Get(ctx, cluster.Name, metav1.GetOptions{})
+					conditions.MarkTrue(cluster, "Ready")
+					cluster, err = server.kcpClient.WorkloadV1alpha1().WorkloadClusters().UpdateStatus(ctx, cluster, metav1.UpdateOptions{})
+					return err == nil
+				}, wait.ForeverTestTimeout, time.Millisecond*100)
+				go wait.UntilWithContext(ctx, func(ctx context.Context) {
+					patchBytes := []byte(fmt.Sprintf(`[{"op":"replace","path":"/status/lastSyncerHeartbeatTime","value":%q}]`, time.Now().Format(time.RFC3339)))
+					_, err := server.kcpClient.WorkloadV1alpha1().WorkloadClusters().Patch(ctx, cluster.Name, types.JSONPatchType, patchBytes, metav1.PatchOptions{}, "status")
+					if err != nil {
+						klog.Errorf("failed to set status.lastSyncerHeartbeatTime: %v", err)
+						return
+					}
+				}, time.Second*10)
+
+				t.Log("Create a new unique sheriff CRD")
+				group := uuid.New().String() + ".io"
+				crd := newSheriffCRD(group)
+				gvr := schema.GroupVersionResource{Group: crd.Spec.Group, Resource: "sheriffs", Version: "v1"}
+				err = configcrds.CreateSingle(ctx, crdClusterClient.Cluster(server.clusterName).ApiextensionsV1().CustomResourceDefinitions(), crd)
+				require.NoError(t, err, "error bootstrapping CRD %s in cluster %s", crd.Name, server.clusterName)
+				require.Eventually(t, func() bool {
+					_, err := dynamicClusterClient.Cluster(server.clusterName).Resource(gvr).Namespace("").List(ctx, metav1.ListOptions{})
+					return err == nil
+				}, wait.ForeverTestTimeout, time.Millisecond*100, "failed to see CRD in cluster")
+
+				t.Log("Create a sheriff and wait for it to be scheduled")
+				_, err = dynamicClusterClient.Cluster(server.clusterName).Resource(gvr).Namespace("default").Create(ctx, newSheriff(group, "woody"), metav1.CreateOptions{})
+				require.NoError(t, err, "failed to create sheriff")
+				require.Eventually(t, func() bool {
+					obj, err := dynamicClusterClient.Cluster(server.clusterName).Resource(gvr).Namespace("default").Get(ctx, "woody", metav1.GetOptions{})
+					if err != nil {
+						return false
+					}
+					require.NoError(t, err, "failed to get sheriff")
+					return obj.GetLabels()[clusterLabel] != ""
+				}, wait.ForeverTestTimeout, time.Millisecond*100, "failed to see sheriff scheduled")
+
+				t.Log("Delete the sheriff and the sheriff CRD")
+				err = dynamicClusterClient.Cluster(server.clusterName).Resource(gvr).Namespace("default").Delete(ctx, "woody", metav1.DeleteOptions{})
+				require.NoError(t, err, "failed to delete sheriff")
+				err = crdClusterClient.Cluster(server.clusterName).ApiextensionsV1().CustomResourceDefinitions().Delete(ctx, crd.Name, metav1.DeleteOptions{})
+				require.NoError(t, err, "failed to delete CRD")
+
+				time.Sleep(30 * time.Second)
+
+				t.Log("Recreate the CRD, and then quickly a namespace, a configmap and a CR whose CRD was just recreated")
+				err = configcrds.CreateSingle(ctx, crdClusterClient.Cluster(server.clusterName).ApiextensionsV1().CustomResourceDefinitions(), crd)
+				require.NoError(t, err, "error bootstrapping CRD %s in cluster %s", crd.Name, server.clusterName)
+				_, err = kubeClusterClient.Cluster(server.clusterName).CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace-test"}}, metav1.CreateOptions{})
+				require.NoError(t, err, "failed to create namespace")
+				_, err = dynamicClusterClient.Cluster(server.clusterName).Resource(gvr).Namespace("default").Create(ctx, newSheriff(group, "lucky-luke"), metav1.CreateOptions{})
+				require.NoError(t, err, "failed to create sheriff")
+
+				t.Log("Create a configmap and wait for it to be scheduled")
+				_, err = kubeClusterClient.Cluster(server.clusterName).CoreV1().ConfigMaps("namespace-test").Create(ctx, &corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "configmap-test"}}, metav1.CreateOptions{})
+				require.NoError(t, err, "failed to create configmap")
+				require.Eventually(t, func() bool {
+					obj, err := kubeClusterClient.Cluster(server.clusterName).CoreV1().ConfigMaps("namespace-test").Get(ctx, "configmap-test", metav1.GetOptions{})
+					if err != nil {
+						klog.Errorf("failed to get configmap: %v", err)
+						return false
+					}
+					return obj.GetLabels()[clusterLabel] != ""
+				}, wait.ForeverTestTimeout, time.Millisecond*100, "failed to see configmap scheduled")
+
+				t.Log("Now also the sheriff should be scheduled")
+				require.Eventually(t, func() bool {
+					obj, err := dynamicClusterClient.Cluster(server.clusterName).Resource(gvr).Namespace("default").Get(ctx, "lucky-luke", metav1.GetOptions{})
+					if err != nil {
+						klog.Errorf("failed to get sheriff: %v", err)
+						return false
+					}
+					return obj.GetLabels()[clusterLabel] != ""
+				}, time.Second*10, time.Millisecond*100, "failed to see sheriff scheduled")
 			},
 		},
 	}
@@ -237,4 +341,46 @@ func expectNamespaces(ctx context.Context, t *testing.T, client kubernetes.Inter
 			return expectErr == nil, expectErr
 		}, 30*time.Second)
 	}, nil
+}
+
+func newSheriffCRD(group string) *apiextensionsv1.CustomResourceDefinition {
+	return &apiextensionsv1.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("sheriffs.%s", group),
+		},
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: group,
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
+				Plural:   "sheriffs",
+				Singular: "sheriff",
+				Kind:     "Sheriff",
+				ListKind: "SheriffList",
+			},
+			Scope: "Namespaced",
+			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1",
+					Served:  true,
+					Storage: true,
+					Schema: &apiextensionsv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+							Type: "object",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func newSheriff(group string, name string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": group + "/v1",
+			"kind":       "Sheriff",
+			"metadata": map[string]interface{}{
+				"name": name,
+			},
+		},
+	}
 }
