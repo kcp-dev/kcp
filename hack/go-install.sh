e3ebf758d048485cd7469ccf0208005bdd96a27d
Steve Kuznetsov <skuznets@redhat.com>
2022-05-23 07:32:17 -0700

    hack: check for missing parameters correctly
    `nounset` is an extremely valuable flag for ensuring that a shell script
    does what is expected. When checking for missing parameters, simply use
    the default substitution syntax so that the flag is not tripped.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/go-install.sh
+++ hack/go-install.sh
@@ -21,22 +21,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "${1}" ]; then
+if [[ -z "${1:-}" ]]; then
   echo "must provide module as first parameter"
   exit 1
 fi
 
-if [ -z "${2}" ]; then
+if [[ -z "${2:-}" ]]; then
   echo "must provide binary name as second parameter"
   exit 1
 fi
 
-if [ -z "${3}" ]; then
+if [[ -z "${3:-}" ]]; then
   echo "must provide version as third parameter"
   exit 1
 fi
 
-if [ -z "${GOBIN}" ]; then
+if [[ -z "${GOBIN:-}" ]]; then
   echo "GOBIN is not set. Must set GOBIN to install the bin in a specified directory."
   exit 1
 fi
