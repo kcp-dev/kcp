1d0eb6336b1379afc56544da6a52a97d2c2470a2
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-07-11 09:01:31 +0200

    apis/tenancy: same schema for CWT references as for CWT names

--- pkg/apis/tenancy/v1alpha1/types.go
+++ pkg/apis/tenancy/v1alpha1/types.go
@@ -130,7 +130,7 @@ type ClusterWorkspaceTypeReference struct {
 
 // ClusterWorkspaceTypeName is a name of a ClusterWorkspaceType
 //
-// +kubebuilder:validation:Pattern=`^[a-z][a-z0-9]+$`
+// +kubebuilder:validation:Pattern=`^[a-z]([a-z0-9-]{0,61}[a-z0-9])?`
 type ClusterWorkspaceTypeName string
 
 func (r ClusterWorkspaceTypeReference) String() string {
