55acde8138e41c628d4aeefedb842d6155871c81
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2022-09-02 15:57:26 +0200

    add feature gates to kubectl kcp
    cli plugin featuregates
    
    kcp plugin featu

--- pkg/cliplugins/workload/plugin/sync_test.go
+++ pkg/cliplugins/workload/plugin/sync_test.go
@@ -180,6 +180,165 @@ spec:
 	require.Empty(t, cmp.Diff(expectedYAML, string(actualYAML)))
 }
 
+func TestNewSyncerYAMLWithFeatureGates(t *testing.T) {
+	expectedYAML := `---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k
+  labels:
+    workload.kcp.io/logical-cluster: root_default_foo
+    workload.kcp.io/sync-target: sync-target-name
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k
+  namespace: kcp-syncer-sync-target-name-34b23c4k
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k-token
+  namespace: kcp-syncer-sync-target-name-34b23c4k
+  annotations:
+    kubernetes.io/service-account.name: kcp-syncer-sync-target-name-34b23c4k
+type: kubernetes.io/service-account-token
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k
+rules:
+- apiGroups:
+  - ""
+  resources:
+  - namespaces
+  verbs:
+  - "create"
+  - "list"
+  - "watch"
+  - "delete"
+- apiGroups:
+  - "apiextensions.k8s.io"
+  resources:
+  - customresourcedefinitions
+  verbs:
+  - "get"
+  - "watch"
+  - "list"
+- apiGroups:
+  - ""
+  resources:
+  - resource1
+  - resource2
+  verbs:
+  - "*"
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: kcp-syncer-sync-target-name-34b23c4k
+subjects:
+- kind: ServiceAccount
+  name: kcp-syncer-sync-target-name-34b23c4k
+  namespace: kcp-syncer-sync-target-name-34b23c4k
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k
+  namespace: kcp-syncer-sync-target-name-34b23c4k
+stringData:
+  kubeconfig: |
+    apiVersion: v1
+    kind: Config
+    clusters:
+    - name: default-cluster
+      cluster:
+        certificate-authority-data: ca-data
+        server: server-url
+    contexts:
+    - name: default-context
+      context:
+        cluster: default-cluster
+        namespace: kcp-namespace
+        user: default-user
+    current-context: default-context
+    users:
+    - name: default-user
+      user:
+        token: token
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: kcp-syncer-sync-target-name-34b23c4k
+  namespace: kcp-syncer-sync-target-name-34b23c4k
+spec:
+  replicas: 1
+  strategy:
+    type: Recreate
+  selector:
+    matchLabels:
+      app: kcp-syncer-sync-target-name-34b23c4k
+  template:
+    metadata:
+      labels:
+        app: kcp-syncer-sync-target-name-34b23c4k
+    spec:
+      containers:
+      - name: kcp-syncer
+        command:
+        - /ko-app/syncer
+        args:
+        - --from-kubeconfig=/kcp/kubeconfig
+        - --sync-target-name=sync-target-name
+        - --sync-target-uid=sync-target-uid
+        - --from-cluster=root:default:foo
+        - --resources=resource1
+        - --resources=resource2
+        - --qps=123.4
+        - --burst=456
+        - --feature-gates=myfeature=true
+        image: image
+        imagePullPolicy: IfNotPresent
+        terminationMessagePolicy: FallbackToLogsOnError
+        volumeMounts:
+        - name: kcp-config
+          mountPath: /kcp/
+          readOnly: true
+      serviceAccountName: kcp-syncer-sync-target-name-34b23c4k
+      volumes:
+        - name: kcp-config
+          secret:
+            secretName: kcp-syncer-sync-target-name-34b23c4k
+            optional: false
+`
+	actualYAML, err := renderSyncerResources(templateInput{
+		ServerURL:          "server-url",
+		Token:              "token",
+		CAData:             "ca-data",
+		KCPNamespace:       "kcp-namespace",
+		Namespace:          "kcp-syncer-sync-target-name-34b23c4k",
+		LogicalCluster:     "root:default:foo",
+		SyncTarget:         "sync-target-name",
+		SyncTargetUID:      "sync-target-uid",
+		Image:              "image",
+		Replicas:           1,
+		ResourcesToSync:    []string{"resource1", "resource2"},
+		QPS:                123.4,
+		Burst:              456,
+		FeatureGatesString: "myfeature=true",
+	}, "kcp-syncer-sync-target-name-34b23c4k")
+	require.NoError(t, err)
+	require.Empty(t, cmp.Diff(expectedYAML, string(actualYAML)))
+}
+
 func TestGetGroupMappings(t *testing.T) {
 	testCases := []struct {
 		name     string
