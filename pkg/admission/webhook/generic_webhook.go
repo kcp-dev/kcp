9b576328e530124d0fbcbbc3d136090fb5d1e07f
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2022-09-20 10:03:48 +0200

    pkg/admission/webhook: differentiate states
    Only if a bound API export is set but no bound API export workspace we want to log.
    This fixes it.

--- pkg/admission/webhook/generic_webhook.go
+++ pkg/admission/webhook/generic_webhook.go
@@ -96,12 +96,13 @@ func (p *WebhookDispatcher) getAPIBindingWorkspace(attr admission.Attributes, cl
 	for _, obj := range objs {
 		apiBinding := obj.(*apisv1alpha1.APIBinding)
 		for _, br := range apiBinding.Status.BoundResources {
-			if apiBinding.Status.BoundAPIExport == nil || apiBinding.Status.BoundAPIExport.Workspace == nil {
+			if apiBinding.Status.BoundAPIExport != nil && apiBinding.Status.BoundAPIExport.Workspace == nil {
 				// this will never happen today. But as soon as we add other reference types (like exports), this log output will remind out of necessary work here.
 				klog.Errorf("APIBinding %s has no referenced workspace", clusterName, apiBinding.Name)
 				continue
 			}
-			if br.Group == attr.GetResource().Group && br.Resource == attr.GetResource().Resource {
+			if apiBinding.Status.BoundAPIExport != nil && apiBinding.Status.BoundAPIExport.Workspace != nil &&
+				br.Group == attr.GetResource().Group && br.Resource == attr.GetResource().Resource {
 				return logicalcluster.New(apiBinding.Status.BoundAPIExport.Workspace.Path), true, nil
 			}
 		}
