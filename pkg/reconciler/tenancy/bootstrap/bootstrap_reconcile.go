81ccbae1c5e957a4485b460271150aac88794b13
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-11-12 20:44:48 +0100

    reconciler/tenancy/bootstrap: port to ThisWorkspace

--- pkg/reconciler/tenancy/bootstrap/bootstrap_reconcile.go
+++ pkg/reconciler/tenancy/bootstrap/bootstrap_reconcile.go
@@ -28,7 +28,7 @@ import (
 	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
 )
 
-func (c *controller) reconcile(ctx context.Context, workspace *tenancyv1alpha1.ClusterWorkspace) error {
+func (c *controller) reconcile(ctx context.Context, workspace *tenancyv1alpha1.ThisWorkspace) error {
 	logger := klog.FromContext(ctx)
 	if workspace.Status.Phase != tenancyv1alpha1.WorkspacePhaseInitializing {
 		return nil
@@ -41,12 +41,12 @@ func (c *controller) reconcile(ctx context.Context, workspace *tenancyv1alpha1.C
 	}
 
 	// bootstrap resources
-	wsClusterName := logicalcluster.From(workspace).Join(workspace.Name)
-	logger.Info("bootstrapping resources for org workspace", "logicalCluster", wsClusterName)
+	clusterName := logicalcluster.From(workspace)
+	logger.Info("bootstrapping resources for workspace", "cluster", clusterName)
 	bootstrapCtx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*30)) // to not block the controller
 	defer cancel()
 
-	if err := c.bootstrap(bootstrapCtx, c.kcpClusterClient.Cluster(wsClusterName).Discovery(), c.dynamicClusterClient.Cluster(wsClusterName), c.kcpClusterClient.Cluster(wsClusterName), c.batteriesIncluded); err != nil {
+	if err := c.bootstrap(bootstrapCtx, c.kcpClusterClient.Cluster(clusterName).Discovery(), c.dynamicClusterClient.Cluster(clusterName), c.kcpClusterClient.Cluster(clusterName), c.batteriesIncluded); err != nil {
 		return err // requeue
 	}
 
