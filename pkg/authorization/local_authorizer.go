a29e65076c232232aaf38247ead211cecf3d325b
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2022-11-14 14:27:38 +0100

    kcp:localauthorizer: rm unused variable

--- pkg/authorization/local_authorizer.go
+++ pkg/authorization/local_authorizer.go
@@ -23,7 +23,6 @@ import (
 	kcpkubernetesinformers "github.com/kcp-dev/client-go/informers"
 	rbacv1listers "github.com/kcp-dev/client-go/listers/rbac/v1"
 
-	"k8s.io/apimachinery/pkg/util/sets"
 	kaudit "k8s.io/apiserver/pkg/audit"
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
@@ -61,8 +60,6 @@ func (a *LocalAuthorizer) RulesFor(user user.Info, namespace string) ([]authoriz
 	// TODO: wire context in RulesFor interface
 	panic("implement me")
 }
-
-var readOnlyVerbs = sets.NewString("get", "list", "watch")
 
 func (a *LocalAuthorizer) Authorize(ctx context.Context, attr authorizer.Attributes) (authorized authorizer.Decision, reason string, err error) {
 	cluster := genericapirequest.ClusterFrom(ctx)
