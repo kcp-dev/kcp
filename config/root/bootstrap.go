7289eee98c7650af9902f6cd36e1bcd9d5d7cfde
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-11-19 23:10:22 +0100

    authorization/workspace_content_authorizer: port to in-cluster auth

--- config/root/bootstrap.go
+++ config/root/bootstrap.go
@@ -19,12 +19,18 @@ package root
 import (
 	"context"
 	"embed"
+	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/klog/v2"
 
 	confighelpers "github.com/kcp-dev/kcp/config/helpers"
+	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
+	kcpclient "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
 )
 
 //go:embed *.yaml
@@ -33,7 +39,7 @@ var fs embed.FS
 // Bootstrap creates resources in this package by continuously retrying the list.
 // This is blocking, i.e. it only returns (with error) when the context is closed or with nil when
 // the bootstrapping is successfully completed.
-func Bootstrap(ctx context.Context, rootDiscoveryClient discovery.DiscoveryInterface, rootDynamicClient dynamic.Interface, homeWorkspaceCreatorGroups []string, batteriesIncluded sets.String) error {
+func Bootstrap(ctx context.Context, kcpClient kcpclient.Interface, rootDiscoveryClient discovery.DiscoveryInterface, rootDynamicClient dynamic.Interface, homeWorkspaceCreatorGroups []string, batteriesIncluded sets.String) error {
 	homeWorkspaceCreatorGroupReplacement := ""
 	for _, group := range homeWorkspaceCreatorGroups {
 		homeWorkspaceCreatorGroupReplacement += `
@@ -45,7 +51,28 @@ func Bootstrap(ctx context.Context, rootDiscoveryClient discovery.DiscoveryInter
 		homeWorkspaceCreatorGroupReplacement = "[]"
 	}
 
-	return confighelpers.Bootstrap(ctx, rootDiscoveryClient, rootDynamicClient, batteriesIncluded, fs, confighelpers.ReplaceOption(
+	if err := confighelpers.Bootstrap(ctx, rootDiscoveryClient, rootDynamicClient, batteriesIncluded, fs, confighelpers.ReplaceOption(
 		"HOME_CREATOR_GROUPS", homeWorkspaceCreatorGroupReplacement,
-	))
+	)); err != nil {
+		return err
+	}
+
+	// set ThisWorkspace to Ready
+	return wait.PollImmediateUntilWithContext(ctx, time.Millisecond*100, func(ctx context.Context) (done bool, err error) {
+		logger := klog.FromContext(ctx).WithValues("bootstrapping", "root-phase0")
+		this, err := kcpClient.TenancyV1alpha1().ThisWorkspaces().Get(ctx, tenancyv1alpha1.ThisWorkspaceName, metav1.GetOptions{})
+		if err != nil {
+			logger.Error(err, "failed to get this workspace in root")
+			return false, nil
+		}
+		if this.Status.Phase == tenancyv1alpha1.WorkspacePhaseInitializing {
+			this.Status.Phase = tenancyv1alpha1.WorkspacePhaseReady
+			_, err = kcpClient.TenancyV1alpha1().ThisWorkspaces().UpdateStatus(ctx, this, metav1.UpdateOptions{})
+			if err != nil {
+				logger.Error(err, "failed to update this workspace status in root")
+				return false, nil
+			}
+		}
+		return true, nil
+	})
 }
