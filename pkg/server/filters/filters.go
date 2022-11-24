48849f5c676814d272b05f9def65a6c9b37cc9fa
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2022-11-24 10:29:36 +0100

    kcp: change allowed cluster names

--- pkg/server/filters/filters.go
+++ pkg/server/filters/filters.go
@@ -49,7 +49,7 @@ const (
 )
 
 var (
-	reClusterName = regexp.MustCompile(`^([a-z]([a-z0-9-]{0,61}[a-z0-9])?:)*[a-z]([a-z0-9-]{0,61}[a-z0-9])?$`)
+	reClusterName = regexp.MustCompile(`^([a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?:)*[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?$`)
 
 	errorScheme = runtime.NewScheme()
 	errorCodecs = serializer.NewCodecFactory(errorScheme)
