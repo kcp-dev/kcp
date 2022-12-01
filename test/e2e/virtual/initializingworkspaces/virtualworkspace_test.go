8ded4e6f6fef59c464dec1ed96f58aa3b0b01b59
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2022-12-01 10:15:27 +0100

    SQUASH: e2e/virtual/initializingworkspaces: port to ThisWorkspace

--- test/e2e/virtual/initializingworkspaces/virtualworkspace_test.go
+++ test/e2e/virtual/initializingworkspaces/virtualworkspace_test.go
@@ -508,7 +508,7 @@ func TestInitializingWorkspacesVirtualWorkspaceAccess(t *testing.T) {
 		patchBytes = patchBytesFor(this, func(workspace *tenancyv1alpha1.ThisWorkspace) {
 			workspace.Status.Initializers = initialization.EnsureInitializerAbsent(initialization.InitializerForType(clusterWorkspaceTypes[initializer]), workspace.Status.Initializers)
 		})
-		this, err = clusterClient.Cluster(logicalcluster.From(ws).Join(ws.Name)).Patch(ctx, tenancyv1alpha1.ThisWorkspaceName, types.MergePatchType, patchBytes, metav1.PatchOptions{}, "status")
+		_, err = clusterClient.Cluster(logicalcluster.From(ws).Join(ws.Name)).Patch(ctx, tenancyv1alpha1.ThisWorkspaceName, types.MergePatchType, patchBytes, metav1.PatchOptions{}, "status")
 		require.NoError(t, err)
 	}
 
