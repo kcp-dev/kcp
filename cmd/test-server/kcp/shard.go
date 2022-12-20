b7ec85dac409542829e45a1ad2c14d729973005b
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-12-20 14:31:52 +0100

    sharded-test-server: restore sub-command output

--- cmd/test-server/kcp/shard.go
+++ cmd/test-server/kcp/shard.go
@@ -123,10 +123,6 @@ func (s *Shard) Start(ctx context.Context) error {
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = s.writer
 
-	if !logger.V(10).Enabled() { // someone has to *really* want verbose logs to get this spam
-		s.writer.StopOut()
-	}
-
 	if err := cmd.Start(); err != nil {
 		return err
 	}
