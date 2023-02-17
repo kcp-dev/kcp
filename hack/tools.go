e9dbffc9bb7ae0bea1d612ec87c016a49e6f1c42
Antonin Stefanutti <antonin@stefanutti.fr>
2023-02-17 16:01:00 +0100

    hack: Add applyconfiguration-gen to tools.go

--- hack/tools.go
+++ hack/tools.go
@@ -21,6 +21,7 @@ package tools
 
 // This package imports things required by this repository, to force `go mod` to see them as dependencies
 import (
+	_ "k8s.io/code-generator/cmd/applyconfiguration-gen"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
