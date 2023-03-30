c1304b34873ec0ba847f4d12e01c80ef3479457c
rahulii <r.sawra@gmail.com>
2023-03-30 11:35:38 +0530

    fix test case

--- pkg/reconciler/workload/synctargetexports/synctargetcompatible_reconcile_test.go
+++ pkg/reconciler/workload/synctargetexports/synctargetcompatible_reconcile_test.go
@@ -84,7 +84,7 @@ func TestSyncTargetCompatibleReconcile(t *testing.T) {
 				}),
 			},
 			wantSyncedResources: []workloadv1alpha1.ResourceToSync{
-				{GroupResource: apisv1alpha1.GroupResource{Group: "apps", Resource: "deployments"}, Versions: []string{"v1"}, State: workloadv1alpha1.ResourceSchemaIncompatibleState},
+				{GroupResource: apisv1alpha1.GroupResource{Group: "apps", Resource: "deployments"}, Versions: []string{"v1"}, State: workloadv1alpha1.ResourceSchemaPendingState},
 			},
 		},
 		{
