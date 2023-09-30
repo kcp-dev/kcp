2defdf288992bb62defe2d5712121bb568e9e4a0
Mangirdas Judeikis <mangirdas@judeikis.lt>
2023-09-30 13:41:55 +0300

    EASY: admission/apiresourceschema: adapt to new validation fields
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- pkg/admission/apiresourceschema/validation_test.go
+++ pkg/admission/apiresourceschema/validation_test.go
@@ -24,6 +24,8 @@ import (
 func TestValidationOptionDrift(t *testing.T) {
 	expectedNonBool := map[string]reflect.Kind{
 		"DisallowDefaultsReason": reflect.String,
+		"CELEnvironmentSet":      reflect.Ptr,
+		"PreexistingExpressions": reflect.Struct,
 	}
 	expectedFalse := map[string]bool{
 		"RequireImmutableNames": true,
