35cd2267c2abc0cfd769347d446f4f01da532110
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2024-09-19 20:11:33 +0200

    cache/replication: fix comparison whether update is needed by removing meta field
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- pkg/reconciler/cache/replication/replication_reconcile_unstructured.go
+++ pkg/reconciler/cache/replication/replication_reconcile_unstructured.go
@@ -17,57 +17,72 @@ limitations under the License.
 package replication
 
 import (
+	"context"
 	"fmt"
 	"reflect"
+
+	"github.com/google/go-cmp/cmp"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	genericrequest "k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/klog/v2"
 )
 
 // ensureMeta changes unstructuredCacheObject's metadata to match unstructuredLocalObject's metadata except the ResourceVersion and the shard annotation fields.
-func ensureMeta(cacheObject *unstructured.Unstructured, localObject *unstructured.Unstructured) (changed bool, err error) {
+func ensureMeta(cacheObject *unstructured.Unstructured, localObject *unstructured.Unstructured) (changed bool, reason string, err error) {
 	cacheObjMetaRaw, hasCacheObjMetaRaw, err := unstructured.NestedFieldNoCopy(cacheObject.Object, "metadata")
 	if err != nil {
-		return false, err
+		return false, "", err
 	}
 	cacheObjMeta, ok := cacheObjMetaRaw.(map[string]interface{})
 	if !ok {
-		return false, fmt.Errorf("metadata field of unstructuredCacheObject is of the type %T, expected map[string]interface{}", cacheObjMetaRaw)
+		return false, "", fmt.Errorf("metadata field of unstructuredCacheObject is of the type %T, expected map[string]interface{}", cacheObjMetaRaw)
 	}
 	localObjMetaRaw, hasLocalObjMetaRaw, err := unstructured.NestedFieldNoCopy(localObject.Object, "metadata")
 	if err != nil {
-		return false, err
+		return false, "", err
 	}
 	localObjMeta, ok := localObjMetaRaw.(map[string]interface{})
 	if !ok {
-		return false, fmt.Errorf("metadata field of unstructuredLocalObjectMeta is of the type %T, expected map[string]interface{}", localObjMetaRaw)
+		return false, "", fmt.Errorf("metadata field of unstructuredLocalObjectMeta is of the type %T, expected map[string]interface{}", localObjMetaRaw)
 	}
 	if !hasLocalObjMetaRaw && !hasCacheObjMetaRaw {
-		return false, nil // no-op
+		return false, "", nil // no-op
 	}
 	if !hasLocalObjMetaRaw {
 		unstructured.RemoveNestedField(cacheObject.Object, "metadata")
-		return true, nil
+		return true, "local .metadata disappeared", nil
 	}
 
-	// before we can compare the cache object we need to
-	// store, remove and then bring back fields that are unique only to the cache object
-	if cacheObjRV, found := cacheObjMeta["resourceVersion"]; found {
-		unstructured.RemoveNestedField(cacheObjMeta, "resourceVersion")
-		defer func() {
-			if err == nil {
-				err = unstructured.SetNestedField(cacheObject.Object, cacheObjRV, "metadata", "resourceVersion")
-			}
-		}()
+	// before we can compare the local and cache objects we need to remove certain
+	// field we know will be different, and bring them back after the comparison.
+	for _, pth := range []string{"resourceVersion", "generation", "managedFields"} {
+		pth := pth
+		if v, found := cacheObjMeta[pth]; found {
+			delete(cacheObjMeta, pth)
+			defer func() { //nolint:gocritic
+				if err == nil {
+					err = unstructured.SetNestedField(cacheObject.Object, v, "metadata", pth)
+				}
+			}()
+		}
+		if v, found := localObjMeta[pth]; found {
+			delete(localObjMeta, pth)
+			defer func() { //nolint:gocritic
+				if err == nil {
+					err = unstructured.SetNestedField(localObject.Object, v, "metadata", pth)
+				}
+			}()
+		}
 	}
 	if cacheObjAnnotationsRaw, found := cacheObjMeta["annotations"]; found {
 		cacheObjAnnotations, ok := cacheObjAnnotationsRaw.(map[string]interface{})
 		if !ok {
-			return false, fmt.Errorf("metadata.annotations field of unstructuredCacheObject is of the type %T, expected map[string]interface{}", cacheObjAnnotationsRaw)
+			return false, "", fmt.Errorf("metadata.annotations field of unstructuredCacheObject is of the type %T, expected map[string]interface{}", cacheObjAnnotationsRaw)
 		}
 		if shard, hasShard := cacheObjAnnotations[genericrequest.ShardAnnotationKey]; hasShard {
-			unstructured.RemoveNestedField(cacheObjAnnotations, genericrequest.ShardAnnotationKey)
+			delete(cacheObjAnnotations, genericrequest.ShardAnnotationKey)
 			defer func() {
 				if err == nil {
 					err = unstructured.SetNestedField(cacheObject.Object, shard, "metadata", "annotations", genericrequest.ShardAnnotationKey)
@@ -77,35 +92,29 @@ func ensureMeta(cacheObject *unstructured.Unstructured, localObject *unstructure
 		// TODO: in the future the original RV will be stored in an annotation
 	}
 
-	// before we can compare with the local object we need to
-	// store, remove and then bring back the ResourceVersion on the local object
-	if localObjRV, found := localObjMeta["resourceVersion"]; found {
-		unstructured.RemoveNestedField(localObjMeta, "resourceVersion")
-		defer func() {
-			if err == nil {
-				localObjMeta["resourceVersion"] = localObjRV
-			}
-		}()
-	}
-
 	changed = !reflect.DeepEqual(cacheObjMeta, localObjMeta)
 	if !changed {
-		return false, nil
+		return false, "", nil
+	}
+
+	reason = ".metadata changed"
+	if logger := klog.FromContext(context.Background()).V(5); logger.Enabled() {
+		reason += " " + cmp.Diff(localObjMeta, cacheObjMeta)
 	}
 
 	newCacheObjMeta := map[string]interface{}{}
 	for k, v := range localObjMeta {
 		newCacheObjMeta[k] = v
 	}
-	return true, unstructured.SetNestedMap(cacheObject.Object, newCacheObjMeta, "metadata")
+	return true, reason, unstructured.SetNestedMap(cacheObject.Object, newCacheObjMeta, "metadata")
 }
 
 // ensureRemaining changes unstructuredCacheObject to match unstructuredLocalObject except for the metadata field
 // returns true when the unstructuredCacheObject was updated.
-func ensureRemaining(cacheObject *unstructured.Unstructured, localObject *unstructured.Unstructured) (bool, error) {
+func ensureRemaining(cacheObject *unstructured.Unstructured, localObject *unstructured.Unstructured) (bool, string, error) {
 	cacheObjMeta, found, err := unstructured.NestedFieldNoCopy(cacheObject.Object, "metadata")
 	if err != nil {
-		return false, err
+		return false, "", err
 	}
 	if found {
 		unstructured.RemoveNestedField(cacheObject.Object, "metadata")
@@ -116,7 +125,7 @@ func ensureRemaining(cacheObject *unstructured.Unstructured, localObject *unstru
 
 	localObjMeta, found, err := unstructured.NestedFieldNoCopy(localObject.Object, "metadata")
 	if err != nil {
-		return false, err
+		return false, "", err
 	}
 	if found {
 		unstructured.RemoveNestedField(localObject.Object, "metadata")
@@ -127,7 +136,12 @@ func ensureRemaining(cacheObject *unstructured.Unstructured, localObject *unstru
 
 	changed := !reflect.DeepEqual(cacheObject.Object, localObject.Object)
 	if !changed {
-		return false, nil
+		return false, "", nil
+	}
+
+	reason := "object changed"
+	if logger := klog.FromContext(context.Background()).V(5); logger.Enabled() {
+		reason += " " + cmp.Diff(localObject.Object, cacheObject.Object)
 	}
 
 	newCacheObj := map[string]interface{}{}
@@ -135,7 +149,7 @@ func ensureRemaining(cacheObject *unstructured.Unstructured, localObject *unstru
 		newCacheObj[k] = v
 	}
 	cacheObject.Object = newCacheObj
-	return true, nil
+	return true, reason, nil
 }
 
 func toUnstructured(obj interface{}) (*unstructured.Unstructured, error) {
