57f8a63f33d5df4cc748c992923b8c34a6366fb8
Mangirdas Judeikis <mangirdas@judeikis.lt>
2024-10-17 19:30:26 +0300

    add workspace statuses updater

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -234,6 +234,9 @@ func (s *Server) installControllers(ctx context.Context, controllerConfig *rest.
 		if err := s.installWorkspaceMountsScheduler(ctx, controllerConfig); err != nil {
 			return err
 		}
+		if err := s.installWorkspaceStatusesScheduler(ctx, controllerConfig); err != nil {
+			return err
+		}
 		if err := s.installTenancyLogicalClusterController(ctx, controllerConfig); err != nil {
 			return err
 		}
