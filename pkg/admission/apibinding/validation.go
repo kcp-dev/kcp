d073eea6c2e229f2de64ef5dca86ca3660890620
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2025-01-16 20:18:49 +0100

    apis/apis: clarify that binding.spec.reference.export is never nil b/c validation
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- pkg/admission/apibinding/validation.go
+++ pkg/admission/apibinding/validation.go
@@ -54,8 +54,7 @@ func ValidateAPIBindingUpdate(oldBinding, newBinding *apisv1alpha1.APIBinding) f
 // ValidateAPIBindingReference validates an APIBinding's BindingReference.
 func ValidateAPIBindingReference(reference apisv1alpha1.BindingReference, path *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
-
-	// For now, field "export" is required via OpenAPI. But just in case...
+	
 	if reference.Export == nil {
 		allErrs = append(allErrs, field.Required(path.Child("export"), ""))
 	} else if reference.Export.Name == "" {
