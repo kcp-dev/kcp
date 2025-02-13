2d619fefa6064c6521f6472ec4db3d5ed775730f
Robert Vasek <rvasek01@gmail.com>
2025-02-13 20:21:56 +0100

    Removed v1alpha1 ValidatingAdmissionPolicy
    Kubernetes v1.32 implicitly removes ValidatingAdmissionPolicy and
    ValidatingAdmissionPolicyBinding from admissionregistration.k8s.io/v1alpha
    in accordance with APILifecycle policy.
    
    This commit removes the relevant CRDs, informers and APIExport builtin types.
    
    On-behalf-of: SAP robert.vasek@sap.com
    Signed-off-by: Robert Vasek <robert.vasek@clyso.com>

--- pkg/virtual/apiexport/schemas/builtin/builtin.go
+++ pkg/virtual/apiexport/schemas/builtin/builtin.go
@@ -246,42 +246,10 @@ var BuiltInAPIs = []internalapis.InternalAPI{
 			Singular: "validatingadmissionpolicy",
 			Kind:     "ValidatingAdmissionPolicy",
 		},
-		GroupVersion:  schema.GroupVersion{Group: "admissionregistration.k8s.io", Version: "v1alpha1"},
-		Instance:      &admissionregistrationv1alpha1.ValidatingAdmissionPolicy{},
-		ResourceScope: apiextensionsv1.ClusterScoped,
-		HasStatus:     true,
-	},
-	{
-		Names: apiextensionsv1.CustomResourceDefinitionNames{
-			Plural:   "validatingadmissionpolicies",
-			Singular: "validatingadmissionpolicy",
-			Kind:     "ValidatingAdmissionPolicy",
-		},
 		GroupVersion:  schema.GroupVersion{Group: "admissionregistration.k8s.io", Version: "v1"},
 		Instance:      &admissionregistrationv1.ValidatingAdmissionPolicy{},
 		ResourceScope: apiextensionsv1.ClusterScoped,
 		HasStatus:     true,
-	},
-	{
-		Names: apiextensionsv1.CustomResourceDefinitionNames{
-			Plural:   "validatingadmissionpolicies",
-			Singular: "validatingadmissionpolicy",
-			Kind:     "ValidatingAdmissionPolicy",
-		},
-		GroupVersion:  schema.GroupVersion{Group: "admissionregistration.k8s.io", Version: "v1beta1"},
-		Instance:      &admissionregistrationv1beta1.ValidatingAdmissionPolicy{},
-		ResourceScope: apiextensionsv1.ClusterScoped,
-		HasStatus:     true,
-	},
-	{
-		Names: apiextensionsv1.CustomResourceDefinitionNames{
-			Plural:   "validatingadmissionpolicybindings",
-			Singular: "validatingadmissionpolicybinding",
-			Kind:     "ValidatingAdmissionPolicyBinding",
-		},
-		GroupVersion:  schema.GroupVersion{Group: "admissionregistration.k8s.io", Version: "v1alpha1"},
-		Instance:      &admissionregistrationv1alpha1.ValidatingAdmissionPolicyBinding{},
-		ResourceScope: apiextensionsv1.ClusterScoped,
 	},
 	{
 		Names: apiextensionsv1.CustomResourceDefinitionNames{
