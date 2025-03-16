7ce537182556036e313102682ec7d05e53d37afc
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2025-03-16 16:23:04 +0200

    update nits

--- test/e2e/mounts/mounts_machinery_test.go
+++ test/e2e/mounts/mounts_machinery_test.go
@@ -70,8 +70,6 @@ func TestMountsMachinery(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	t.Cleanup(cancelFunc)
 
-	_, _ = kcptesting.NewWorkspaceFixture(t, server, core.RootCluster.Path())
-
 	// This will create structure as bellow for testing:
 	// └── root
 	//   └── e2e-workspace-n784f
@@ -79,15 +77,12 @@ func TestMountsMachinery(t *testing.T) {
 	//    └── source
 	//        └── mount
 	//
-	orgSource, _ := kcptesting.NewWorkspaceFixture(t, server, core.RootCluster.Path(),
-		kcptesting.WithName("source"),
-		kcptesting.WithType(core.RootCluster.Path(), "organization"))
-	orgDestination, _ := kcptesting.NewWorkspaceFixture(t, server, core.RootCluster.Path(),
-		kcptesting.WithName("destination"),
-		kcptesting.WithType(core.RootCluster.Path(), "organization"))
+	rootOrg, _ := kcptesting.NewWorkspaceFixture(t, server, core.RootCluster.Path())
 
-	sourcePath, _ := kcptesting.NewWorkspaceFixture(t, server, orgSource, kcptesting.WithName("source"))
-	_, destinationWorkspaceObj := kcptesting.NewWorkspaceFixture(t, server, orgDestination, kcptesting.WithName("destination"))
+	sourcePath, _ := kcptesting.NewWorkspaceFixture(t, server, rootOrg,
+		kcptesting.WithName("source"))
+	_, destinationWorkspaceObj := kcptesting.NewWorkspaceFixture(t, server, rootOrg,
+		kcptesting.WithName("destination"))
 
 	cfg := server.BaseConfig(t)
 	kcpClusterClient, err := kcpclientset.NewForConfig(cfg)
@@ -149,7 +144,7 @@ func TestMountsMachinery(t *testing.T) {
 		Resource: "kubeclusters",
 	}
 	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		destinationWorkspace, err := kcpClusterClient.Cluster(orgSource).TenancyV1alpha1().Workspaces().Get(ctx, destinationWorkspaceObj.Name, metav1.GetOptions{})
+		destinationWorkspace, err := kcpClusterClient.Cluster(rootOrg).TenancyV1alpha1().Workspaces().Get(ctx, destinationWorkspaceObj.Name, metav1.GetOptions{})
 		require.NoError(t, err)
 
 		if destinationWorkspace.Spec.URL == "" {
