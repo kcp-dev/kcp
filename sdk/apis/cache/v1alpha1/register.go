fcbc38a5de1fe69e2b16ff4cd74ee4bbdfc394cd
Robert Vasek <rvasek01@gmail.com>
2025-05-28 22:07:42 +0200

    Register CachedResourceEndpointSlice{,List} with sdk/apis/cache/v1alpha1
    On-behalf-of: @SAP robert.vasek@sap.com
    Signed-off-by: Robert Vasek <robert.vasek@clyso.com>

--- sdk/apis/cache/v1alpha1/register.go
+++ sdk/apis/cache/v1alpha1/register.go
@@ -47,6 +47,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&CachedResource{},
 		&CachedResourceList{},
+		&CachedResourceEndpointSlice{},
+		&CachedResourceEndpointSliceList{},
 		&CachedObject{},
 		&CachedObjectList{},
 	)
