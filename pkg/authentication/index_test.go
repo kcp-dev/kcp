53d90e08c42545c480bf39603e9bb4b2006c2196
Christoph Mewes <christoph@kubermatic.com>
2025-08-02 23:06:53 +0200

    cleanup unit test
    On-behalf-of: @SAP christoph.mewes@sap.com

--- pkg/authentication/index_test.go
+++ pkg/authentication/index_test.go
@@ -19,8 +19,9 @@ package authentication
 import (
 	"context"
 	"errors"
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -32,8 +33,11 @@ import (
 	conditionsv1alpha1 "github.com/kcp-dev/kcp/sdk/apis/third_party/conditions/apis/conditions/v1alpha1"
 )
 
-func TestDeleteShard(t *testing.T) {
-	const shardName = "shard-1"
+func TestCrossShardWorkspaceType(t *testing.T) {
+	const (
+		shardName   = "shard-1"
+		teamCluster = "logicalteamcluster"
+	)
 
 	ctx, cancel := context.WithCancelCause(context.Background())
 	defer cancel(errors.New("test has ended"))
@@ -52,170 +56,20 @@ func TestDeleteShard(t *testing.T) {
 	authIndex.UpsertWorkspaceType("root", newWorkspaceType("custom-type", "root"))
 
 	// setup the team workspace (ws is on root shard in root workspace, cluster is on the 2nd shard)
-	ws := newWorkspace("team1", "root", "logicalteamcluster")
+	ws := newWorkspace("team1", "root", teamCluster)
 	ws.Spec.Type = &tenancyv1alpha1.WorkspaceTypeReference{
 		Name: "custom-type",
 		Path: "root",
 	}
 	clusterIndex.UpsertWorkspace("root", ws)
-	clusterIndex.UpsertLogicalCluster(shardName, newLogicalCluster("logicalteamcluster", "root:custom-type"))
+	clusterIndex.UpsertLogicalCluster(shardName, newLogicalCluster(teamCluster, "root:custom-type"))
 
 	r, found := clusterIndex.Lookup(logicalcluster.NewPath("root:team1"))
-	fmt.Printf("result: %+v\n", r)
-	fmt.Printf("found: %v\n", found)
+	require.True(t, found)
+	require.Equal(t, shardName, r.Shard)
+	require.Equal(t, teamCluster, r.Cluster.String())
+	require.Equal(t, "root:custom-type", r.Type.String())
 }
-
-// func TestDeleteLogicalCluster(t *testing.T) {
-// 	target := New(nil)
-
-// 	// ensure deleting not existent logical cluster won't blow up
-// 	target.DeleteLogicalCluster("root", newLogicalCluster("34"))
-
-// 	target.UpsertShard("root", "https://root.io")
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("root"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("34"))
-
-// 	r, found := target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "34", "", true)
-
-// 	// ensure that after deleting the logical cluster it cannot be looked up
-// 	target.DeleteLogicalCluster("root", newLogicalCluster("34"))
-
-// 	r, found = target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "", "", "", false)
-
-// 	r, found = target.Lookup(logicalcluster.NewPath("root"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "root", "", true)
-// }
-
-// func TestDeleteWorkspace(t *testing.T) {
-// 	target := New(nil)
-
-// 	// ensure deleting not existent workspace won't blow up
-// 	target.DeleteWorkspace("root", newWorkspace("org", "root", "34"))
-
-// 	target.UpsertShard("root", "https://root.io")
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "34"))
-// 	target.UpsertWorkspace("root", newWorkspace("org1", "root", "43"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("root"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("43"))
-
-// 	r, found := target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "34", "", true)
-
-// 	target.DeleteWorkspace("root", newWorkspace("org", "root", "34"))
-
-// 	r, found = target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "", "", "", false)
-
-// 	r, found = target.Lookup(logicalcluster.NewPath("root:org1"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "43", "", true)
-// }
-
-// func TestUpsertLogicalCluster(t *testing.T) {
-// 	target := New(nil)
-
-// 	target.UpsertShard("root", "https://root.io")
-// 	target.UpsertShard("amber", "https://amber.io")
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("root"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("34"))
-
-// 	r, found := target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "34", "", true)
-
-// 	target.UpsertLogicalCluster("amber", newLogicalCluster("34"))
-// 	r, found = target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "amber", "34", "", true)
-// }
-
-// // Since LookupURL uses Lookup method the following test is just a smoke tests.
-// func TestLookupURL(t *testing.T) {
-// 	target := New(nil)
-
-// 	target.UpsertShard("root", "https://root.io")
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("root"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("34"))
-
-// 	r, found := target.LookupURL(logicalcluster.NewPath("root:org"))
-// 	if !found {
-// 		t.Fatalf("expected to find a URL for %q path", "root:org")
-// 	}
-// 	if r.URL != "https://root.io/clusters/34" {
-// 		t.Fatalf("unexpected url. returned = %v, expected = %v for %q path", r.URL, "https://root.io/clusters/34", "root:org")
-// 	}
-
-// 	r, found = target.LookupURL(logicalcluster.NewPath("root:org:rh"))
-// 	if found {
-// 		t.Fatalf("didn't expected to find a URL for %q path", "root:org:rh")
-// 	}
-// 	if len(r.URL) > 0 {
-// 		t.Fatalf("unexpected url = %v returned for %q path", r.URL, "root:org:rh")
-// 	}
-// }
-
-// func TestUpsertShard(t *testing.T) {
-// 	target := New(nil)
-
-// 	target.UpsertShard("root", "https://root.io")
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("root"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("34"))
-
-// 	r, found := target.LookupURL(logicalcluster.NewPath("root:org"))
-// 	if !found {
-// 		t.Fatalf("expected to find a URL for %q path", "root:org")
-// 	}
-// 	if r.URL != "https://root.io/clusters/34" {
-// 		t.Fatalf("unexpected url = %v returned, expected = %v for %q path", r.URL, "https://root.io/clusters/34", "root:org")
-// 	}
-
-// 	target.UpsertShard("root", "https://new-root.io")
-// 	r, found = target.LookupURL(logicalcluster.NewPath("root:org"))
-// 	if !found {
-// 		t.Fatalf("expected to find a URL for %q path", "root:org")
-// 	}
-// 	if r.URL != "https://new-root.io/clusters/34" {
-// 		t.Fatalf("unexpected url = %v returned, expected = %v for %q path", r.URL, "https://new-root.io/clusters/34", "root:org")
-// 	}
-// }
-
-// func TestUpsertWorkspace(t *testing.T) {
-// 	target := New(nil)
-
-// 	target.UpsertShard("root", "https://root.io")
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("root"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("34"))
-// 	target.UpsertLogicalCluster("root", newLogicalCluster("44"))
-
-// 	r, found := target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "34", "", true)
-
-// 	target.UpsertWorkspace("root", newWorkspace("org", "root", "44"))
-// 	r, found = target.Lookup(logicalcluster.NewPath("root:org"))
-// 	validateLookupOutput(t, logicalcluster.NewPath("root:org"), r.Shard, r.Cluster, r.URL, found, "root", "44", "", true)
-// }
-
-// func validateLookupOutput(t *testing.T, path logicalcluster.Path, shard string, cluster logicalcluster.Name, url string, found bool, expectedShard string, expectedCluster logicalcluster.Name, expectedURL string, expectToFind bool) {
-// 	t.Helper()
-
-// 	if found != expectToFind {
-// 		t.Fatalf("found != expectedToFind, for %q path", path)
-// 	}
-// 	if cluster != expectedCluster {
-// 		t.Fatalf("unexpected logical cluster = %v, expected = %v for %q path", cluster, expectedCluster, path)
-// 	}
-// 	if shard != expectedShard {
-// 		t.Fatalf("unexpected shard = %v, expected = %v, for %q path", shard, expectedShard, path)
-// 	}
-// 	if url != expectedURL {
-// 		t.Fatalf("unexpected url = %v, expected = %v, for %q path", url, expectedURL, path)
-// 	}
-// }
 
 func newWorkspaceType(name, cluster string) *tenancyv1alpha1.WorkspaceType {
 	return &tenancyv1alpha1.WorkspaceType{
@@ -232,24 +86,8 @@ func newWorkspace(name, cluster, scheduledCluster string) *tenancyv1alpha1.Works
 	}
 }
 
-func newWorkspaceWithMount(name, cluster, scheduledCluster string, ref tenancyv1alpha1.ObjectReference) *tenancyv1alpha1.Workspace {
-	ws := newWorkspace(name, cluster, scheduledCluster)
-	ws.Spec.Mount = &tenancyv1alpha1.Mount{Reference: ref}
-	return ws
-}
-
 func WithCondition(ws *tenancyv1alpha1.Workspace, condition conditionsv1alpha1.Condition) *tenancyv1alpha1.Workspace {
 	ws.Status.Conditions = []conditionsv1alpha1.Condition{condition}
-	return ws
-}
-
-func withPhase(ws *tenancyv1alpha1.Workspace, phase corev1alpha1.LogicalClusterPhaseType) *tenancyv1alpha1.Workspace {
-	ws.Status.Phase = phase
-	return ws
-}
-
-func withURL(ws *tenancyv1alpha1.Workspace, url string) *tenancyv1alpha1.Workspace {
-	ws.Spec.URL = url
 	return ws
 }
 
