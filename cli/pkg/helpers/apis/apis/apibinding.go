fbf12eee5ecc40304849924ed0262efcb1933ff5
Christoph Mewes <christoph@kubermatic.com>
2025-08-15 15:01:31 +0200

    implement wrappers in CLI module to allow working with older API versions
    On-behalf-of: @SAP christoph.mewes@sap.com

---
+++ cli/pkg/helpers/apis/apis/apibinding.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2025 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apis
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	apisv1alpha1 "github.com/kcp-dev/kcp/sdk/apis/apis/v1alpha1"
+	apisv1alpha2 "github.com/kcp-dev/kcp/sdk/apis/apis/v1alpha2"
+	kcpclientset "github.com/kcp-dev/kcp/sdk/client/clientset/versioned"
+)
+
+type APIBinding interface {
+	Name() string
+	Refresh(ctx context.Context, client kcpclientset.Interface) error
+	Create(ctx context.Context, client kcpclientset.Interface) error
+	SetPermissionClaims(claims []apisv1alpha2.AcceptablePermissionClaim) error
+	IsBound() bool
+}
+
+type APIBindingList interface {
+	PrintPermissionClaims(out io.Writer) error
+}
+
+func GetAPIBinding(ctx context.Context, client kcpclientset.Interface, preferredVersion string, name string) (APIBinding, error) {
+	switch preferredVersion {
+	case "v1alpha1":
+		return getAPIBindingV1alpha1(ctx, client, name)
+
+	case "v1alpha2":
+		return getAPIBindingV1alpha2(ctx, client, name)
+
+	default:
+		return nil, fmt.Errorf("version %q is not supported by this plugin", preferredVersion)
+	}
+}
+
+func ListAPIBindings(ctx context.Context, client kcpclientset.Interface, preferredVersion string) (APIBindingList, error) {
+	switch preferredVersion {
+	case "v1alpha1":
+		return listAPIBindingsV1alpha1(ctx, client)
+
+	case "v1alpha2":
+		return listAPIBindingsV1alpha2(ctx, client)
+
+	default:
+		return nil, fmt.Errorf("version %q is not supported by this plugin", preferredVersion)
+	}
+}
+
+func NewAPIBinding(nativeBinding any) APIBinding {
+	switch asserted := nativeBinding.(type) {
+	case *apisv1alpha1.APIBinding:
+		return &apiBindingV1alpha1{binding: asserted}
+	case *apisv1alpha2.APIBinding:
+		return &apiBindingV1alpha2{binding: asserted}
+	}
+
+	panic("Unsupported APIBinding version provided.")
+}
+
+func NewAPIBindingList(nativeBinding any) APIBindingList {
+	switch asserted := nativeBinding.(type) {
+	case *apisv1alpha1.APIBinding:
+		return &apiBindingListV1alpha1{bindings: []*apisv1alpha1.APIBinding{asserted}}
+	case *apisv1alpha2.APIBinding:
+		return &apiBindingListV1alpha2{bindings: []*apisv1alpha2.APIBinding{asserted}}
+	}
+
+	panic("Unsupported APIBinding version provided.")
+}
---
+++ cli/pkg/helpers/apis/apis/apibinding_v1alpha1.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2025 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apis
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"slices"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	apisv1alpha1 "github.com/kcp-dev/kcp/sdk/apis/apis/v1alpha1"
+	apisv1alpha2 "github.com/kcp-dev/kcp/sdk/apis/apis/v1alpha2"
+	kcpclientset "github.com/kcp-dev/kcp/sdk/client/clientset/versioned"
+)
+
+type apiBindingV1alpha1 struct {
+	binding *apisv1alpha1.APIBinding
+}
+
+func getAPIBindingV1alpha1(ctx context.Context, client kcpclientset.Interface, name string) (*apiBindingV1alpha1, error) {
+	binding, err := client.ApisV1alpha1().APIBindings().Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &apiBindingV1alpha1{binding: binding}, nil
+}
+
+func (b *apiBindingV1alpha1) Refresh(ctx context.Context, client kcpclientset.Interface) error {
+	current, err := client.ApisV1alpha1().APIBindings().Get(ctx, b.binding.Name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	b.binding = current
+
+	return nil
+}
+
+func (b *apiBindingV1alpha1) Create(ctx context.Context, client kcpclientset.Interface) error {
+	created, err := client.ApisV1alpha1().APIBindings().Create(ctx, b.binding, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+	b.binding = created
+
+	return nil
+}
+
+func (b *apiBindingV1alpha1) IsBound() bool {
+	return b.binding.Status.Phase == apisv1alpha1.APIBindingPhaseBound
+}
+
+func (b *apiBindingV1alpha1) Name() string {
+	return b.binding.Name
+}
+
+func (b *apiBindingV1alpha1) SetPermissionClaims(claims []apisv1alpha2.AcceptablePermissionClaim) error {
+	alpha1Claims := []apisv1alpha1.AcceptablePermissionClaim{}
+
+	for _, claim := range claims {
+		if len(claim.Verbs) != 0 && !slices.Equal(claim.Verbs, []string{"*"}) {
+			return errors.New("apis v1alpha1 does not support verbs in permission claims")
+		}
+
+		if !claim.Selector.MatchAll {
+			return errors.New("apis v1alpha1 only supports the `all` selector for permission claims")
+		}
+
+		alpha1Claims = append(alpha1Claims, apisv1alpha1.AcceptablePermissionClaim{
+			PermissionClaim: apisv1alpha1.PermissionClaim{
+				GroupResource: apisv1alpha1.GroupResource{
+					Group:    claim.Group,
+					Resource: claim.Resource,
+				},
+				All:          true,
+				IdentityHash: claim.IdentityHash,
+			},
+			State: apisv1alpha1.AcceptablePermissionClaimState(claim.State),
+		})
+	}
+
+	b.binding.Spec.PermissionClaims = alpha1Claims
+	return nil
+}
+
+type apiBindingListV1alpha1 struct {
+	bindings []*apisv1alpha1.APIBinding
+}
+
+func listAPIBindingsV1alpha1(ctx context.Context, client kcpclientset.Interface) (APIBindingList, error) {
+	nativeBindings, err := client.ApisV1alpha1().APIBindings().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	list := &apiBindingListV1alpha1{
+		bindings: make([]*apisv1alpha1.APIBinding, len(nativeBindings.Items)),
+	}
+
+	for idx := range nativeBindings.Items {
+		list.bindings[idx] = &nativeBindings.Items[idx]
+	}
+
+	return list, nil
+}
+
+func (l *apiBindingListV1alpha1) PrintPermissionClaims(out io.Writer) error {
+	columnNames := []string{"APIBINDING", "RESOURCE GROUP-VERSION", "STATUS"}
+	if _, err := fmt.Fprintf(out, "%s\n", strings.Join(columnNames, "\t")); err != nil {
+		return err
+	}
+
+	for _, binding := range l.bindings {
+		for _, claim := range binding.Spec.PermissionClaims {
+			if _, err := fmt.Fprintf(out, "%s\t%s\t%s\n", binding.Name, claim.Group+"-"+claim.Resource, string(claim.State)); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
