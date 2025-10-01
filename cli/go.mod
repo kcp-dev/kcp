9be523f0adc532f987567390e2e89902e1aa519f
olalekan odukoya <odukoyaonline@gmail.com>
2025-10-01 03:00:56 +0100

    add -i flag to workspace command
    Signed-off-by: olalekan odukoya <odukoyaonline@gmail.com>

--- cli/go.mod
+++ cli/go.mod
@@ -3,6 +3,8 @@ module github.com/kcp-dev/kcp/cli
 go 1.24.0
 
 require (
+	github.com/charmbracelet/bubbletea v1.3.10
+	github.com/charmbracelet/lipgloss v1.1.0
 	github.com/google/go-cmp v0.7.0
 	github.com/kcp-dev/client-go v0.0.0-20250728134101-0355faa9361b
 	github.com/kcp-dev/kcp/sdk v0.0.0-00010101000000-000000000000
@@ -22,9 +24,16 @@ require (
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/charmbracelet/colorprofile v0.3.2 // indirect
+	github.com/charmbracelet/x/ansi v0.10.1 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.12.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -42,24 +51,31 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kcp-dev/apimachinery/v2 v2.0.1-0.20250728122101-adbf20db3e51 // indirect
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
-	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
 	github.com/moby/term v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/onsi/gomega v1.35.1 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/oauth2 v0.29.0 // indirect
 	golang.org/x/sync v0.14.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/term v0.32.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
@@ -79,6 +95,8 @@ require (
 )
 
 replace (
+	github.com/charmbracelet/colorprofile => github.com/charmbracelet/colorprofile v0.2.2
+	github.com/charmbracelet/x/ansi => github.com/charmbracelet/x/ansi v0.8.0
 	github.com/kcp-dev/client-go => ../staging/src/github.com/kcp-dev/client-go
 	github.com/kcp-dev/kcp/sdk => ../sdk
 )
---
+++ cli/pkg/workspace/plugin/interactive.go
@@ -0,0 +1,262 @@
+/*
+Copyright 2025 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugin
+
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
+	"github.com/kcp-dev/logicalcluster/v3"
+)
+
+type treeNode struct {
+	name       string
+	path       logicalcluster.Path
+	info       *workspaceInfo
+	expanded   bool
+	selected   bool
+	selectable bool
+	children   []*treeNode
+	parent     *treeNode
+}
+
+type model struct {
+	tree              *treeNode
+	currentNode       *treeNode
+	selectedWorkspace *logicalcluster.Path
+	width             int
+	height            int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c", "esc":
+			return m, tea.Quit
+
+		case "up", "k":
+			m.moveToPrevious()
+			return m, nil
+
+		case "down", "j":
+			m.moveToNext()
+			return m, nil
+
+		case "right", "l", " ":
+			if m.currentNode != nil && !m.currentNode.expanded && len(m.currentNode.children) > 0 {
+				m.currentNode.expanded = true
+			}
+			return m, nil
+
+		case "left", "h", "backspace":
+			if m.currentNode != nil && m.currentNode.expanded {
+				m.currentNode.expanded = false
+			}
+			return m, nil
+
+		case "enter":
+			if m.currentNode != nil && m.currentNode.info != nil {
+				m.selectedWorkspace = &m.currentNode.info.Path
+				return m, tea.Quit
+			}
+			return m, nil
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.width == 0 {
+		return "Loading..."
+	}
+
+	titleStyle := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("170"))
+
+	borderStyle := lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62")).
+		Padding(1, 2).
+		Width(m.width - 4)
+
+	helpStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("241"))
+
+	detailStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("248")).
+		Align(lipgloss.Right)
+
+	title := titleStyle.Render("kcp Workspaces")
+
+	var details string
+	if m.currentNode != nil && m.currentNode.info != nil {
+		detailParts := []string{m.currentNode.info.Path.String()}
+		if m.currentNode.info.Type != nil {
+			typeRef := string(m.currentNode.info.Type.Name)
+			if m.currentNode.info.Type.Path != "" {
+				typeRef = m.currentNode.info.Type.Path + ":" + typeRef
+			}
+			detailParts = append(detailParts, typeRef)
+		}
+		details = detailStyle.Render(strings.Join(detailParts, " | "))
+	}
+
+	headerWidth := m.width - 8
+	titleWidth := lipgloss.Width(title)
+	detailWidth := lipgloss.Width(details)
+	spacerWidth := headerWidth - titleWidth - detailWidth
+	if spacerWidth < 0 {
+		spacerWidth = 0
+	}
+
+	header := lipgloss.JoinHorizontal(lipgloss.Top,
+		title,
+		strings.Repeat(" ", spacerWidth),
+		details,
+	)
+
+	var treeLines []string
+	treeLines = append(treeLines, "")
+	m.flattenTree(m.tree, 0, &treeLines)
+	treeLines = append(treeLines, "")
+
+	help := helpStyle.Render("↑/↓: navigate  →/←: expand/collapse  q: quit")
+
+	var prompt string
+	if m.currentNode != nil && m.currentNode.info != nil {
+		prompt = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("34")).
+			Render("\nPress Enter to switch to this workspace")
+	}
+
+	content := lipgloss.JoinVertical(lipgloss.Left,
+		header,
+		strings.Join(treeLines, "\n"),
+		help,
+		prompt,
+	)
+
+	return borderStyle.Render(content)
+}
+
+func (m *model) flattenTree(node *treeNode, depth int, lines *[]string) {
+	if node == nil {
+		return
+	}
+
+	prefix := strings.Repeat("  ", depth)
+
+	icon := "  "
+	if len(node.children) > 0 {
+		if node.expanded {
+			icon = "▼ "
+		} else {
+			icon = "▶ "
+		}
+	}
+
+	nodeName := prefix + icon + node.name
+	line := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("252")).
+		Render("  " + nodeName)
+
+	if m.currentNode == node {
+		if node.selected {
+			line = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("11")).
+				Background(lipgloss.Color("235")).
+				Bold(true).
+				Render("→ " + nodeName)
+		} else {
+			line = lipgloss.NewStyle().
+				Background(lipgloss.Color("235")).
+				Render("→ " + nodeName)
+		}
+	} else if node.selected {
+		line = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("11")).
+			Render("  " + nodeName)
+	}
+
+	*lines = append(*lines, line)
+
+	if node.expanded {
+		for _, child := range node.children {
+			m.flattenTree(child, depth+1, lines)
+		}
+	}
+}
+
+func (m *model) moveToPrevious() {
+	if m.currentNode == nil {
+		return
+	}
+
+	var allNodes []*treeNode
+	m.collectVisibleNodes(m.tree, &allNodes)
+
+	for i, node := range allNodes {
+		if node == m.currentNode && i > 0 {
+			m.currentNode = allNodes[i-1]
+			return
+		}
+	}
+}
+
+func (m *model) moveToNext() {
+	if m.currentNode == nil {
+		return
+	}
+
+	var allNodes []*treeNode
+	m.collectVisibleNodes(m.tree, &allNodes)
+
+	for i, node := range allNodes {
+		if node == m.currentNode && i < len(allNodes)-1 {
+			m.currentNode = allNodes[i+1]
+			return
+		}
+	}
+}
+
+func (m *model) collectVisibleNodes(node *treeNode, nodes *[]*treeNode) {
+	if node == nil {
+		return
+	}
+
+	*nodes = append(*nodes, node)
+
+	if node.expanded {
+		for _, child := range node.children {
+			m.collectVisibleNodes(child, nodes)
+		}
+	}
+}
