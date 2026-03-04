10bc50ea5ea7240df2e75ff5fabb38444e8ee0c2
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-03-04 15:25:15 +0200

    Add APIResourceSchema VW
    Signed-off-by: Mangirdas Judeikis <mangirdas@judeikis.lt>
    Signed-off-by: Nelo-T. Wallus <mangirdas.judeikis@sap.com>

---
+++ pkg/virtual/apiresourceschema/authorizer/authorizer.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2026 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package authorizer
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apiserver/pkg/authorization/authorizer"
+
+	kcpkubernetesclientset "github.com/kcp-dev/client-go/kubernetes"
+	"github.com/kcp-dev/logicalcluster/v3"
+	apisv1alpha2 "github.com/kcp-dev/sdk/apis/apis/v1alpha2"
+
+	"github.com/kcp-dev/kcp/pkg/authorization/delegated"
+	dynamiccontext "github.com/kcp-dev/kcp/pkg/virtual/framework/dynamic/context"
+)
+
+// apiResourceSchemaAuthorizer checks if the user has permission to access
+// APIResourceSchemas in a consumer workspace. Access is granted if the user
+// has permission to read APIBindings in the consumer workspace.
+//
+// This is designed for provider operators who need to know what schemas
+// consumers are using via their APIBindings.
+type apiResourceSchemaAuthorizer struct {
+	newDelegatedAuthorizer func(clusterName logicalcluster.Name) (authorizer.Authorizer, error)
+}
+
+// NewAPIResourceSchemaAuthorizer creates a new authorizer that checks
+// if the user has read access to APIBindings in the consumer workspace.
+// The consumer workspace is determined from the APIDomainKey in the context.
+//
+// Access is allowed if:
+// - The user has "get", "list", or "watch" permission on "apibindings" in the consumer workspace
+// - The request verb is read-only (get, list, watch).
+func NewAPIResourceSchemaAuthorizer(kubeClusterClient kcpkubernetesclientset.ClusterInterface) authorizer.Authorizer {
+	return &apiResourceSchemaAuthorizer{
+		newDelegatedAuthorizer: func(clusterName logicalcluster.Name) (authorizer.Authorizer, error) {
+			return delegated.NewDelegatedAuthorizer(clusterName, kubeClusterClient, delegated.Options{})
+		},
+	}
+}
+
+func (a *apiResourceSchemaAuthorizer) Authorize(ctx context.Context, attr authorizer.Attributes) (authorizer.Decision, string, error) {
+	// Only allow read-only verbs
+	switch attr.GetVerb() {
+	case "get", "list", "watch":
+		// Continue to authorization check
+	default:
+		return authorizer.DecisionDeny, "only read-only access is allowed to apiresourceschemas", nil
+	}
+
+	// Get the consumer cluster from the API domain key
+	apiDomainKey := dynamiccontext.APIDomainKeyFrom(ctx)
+	if apiDomainKey == "" {
+		return authorizer.DecisionNoOpinion, "", fmt.Errorf("no API domain key in context")
+	}
+
+	consumerCluster := logicalcluster.Name(apiDomainKey)
+	if consumerCluster.Empty() {
+		return authorizer.DecisionNoOpinion, "", fmt.Errorf("invalid consumer cluster name")
+	}
+
+	// Create a delegated authorizer for the consumer cluster
+	authz, err := a.newDelegatedAuthorizer(consumerCluster)
+	if err != nil {
+		return authorizer.DecisionNoOpinion, "",
+			fmt.Errorf("error creating delegated authorizer for consumer cluster %q: %w", consumerCluster, err)
+	}
+
+	// Check if the user has permission to read APIBindings in the consumer workspace
+	// This is the "invitation" - if you can read bindings, you can see what schemas they reference
+	SARAttributes := authorizer.AttributesRecord{
+		APIGroup:        apisv1alpha2.SchemeGroupVersion.Group,
+		APIVersion:      apisv1alpha2.SchemeGroupVersion.Version,
+		User:            attr.GetUser(),
+		Verb:            "get", // Check for get permission as a baseline
+		Resource:        "apibindings",
+		ResourceRequest: true,
+	}
+
+	dec, reason, err := authz.Authorize(ctx, SARAttributes)
+	if err != nil {
+		return authorizer.DecisionNoOpinion, "",
+			fmt.Errorf("error authorizing RBAC in consumer cluster %q: %w", consumerCluster, err)
+	}
+
+	if dec == authorizer.DecisionAllow {
+		return authorizer.DecisionAllow, "", nil
+	}
+
+	return dec, fmt.Sprintf("consumer cluster: %q RBAC decision: %v - user needs permission to read apibindings",
+		consumerCluster, reason), nil
+}
---
+++ pkg/virtual/apiresourceschema/builder/storage.go
@@ -0,0 +1,173 @@
+/*
+Copyright 2026 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package builder
+
+import (
+	"context"
+	"fmt"
+
+	"sigs.k8s.io/structured-merge-diff/v6/fieldpath"
+
+	structuralschema "k8s.io/apiextensions-apiserver/pkg/apiserver/schema"
+	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metainternalversion "k8s.io/apimachinery/pkg/apis/meta/internalversion"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/apiserver/pkg/registry/rest"
+
+	"github.com/kcp-dev/logicalcluster/v3"
+	apisv1alpha1listers "github.com/kcp-dev/sdk/client/listers/apis/v1alpha1"
+
+	"github.com/kcp-dev/kcp/pkg/virtual/framework/dynamic/apiserver"
+	"github.com/kcp-dev/kcp/pkg/virtual/framework/forwardingregistry"
+)
+
+// NewSchemaRestProvider creates a REST provider function that serves APIResourceSchemas
+// from the lister, filtered to only include schemas in the schemaClusterAndNames map.
+func NewSchemaRestProvider(
+	lister apisv1alpha1listers.APIResourceSchemaClusterLister,
+	schemaClusterAndNames map[string]logicalcluster.Name,
+) apiserver.RestProviderFunc {
+	return func(
+		resource schema.GroupVersionResource,
+		kind schema.GroupVersionKind,
+		listKind schema.GroupVersionKind,
+		typer runtime.ObjectTyper,
+		tableConvertor rest.TableConvertor,
+		namespaceScoped bool,
+		schemaValidator validation.SchemaValidator,
+		subresourcesSchemaValidator map[string]validation.SchemaValidator,
+		structuralSchema *structuralschema.Structural,
+	) (mainStorage rest.Storage, subresourceStorages map[string]rest.Storage) {
+		// Create the storage functions
+		factoryFunc := forwardingregistry.FactoryFunc(func() runtime.Object {
+			ret := &unstructured.Unstructured{}
+			ret.SetGroupVersionKind(kind)
+			return ret
+		})
+
+		listFactoryFunc := forwardingregistry.ListFactoryFunc(func() runtime.Object {
+			ret := &unstructured.UnstructuredList{}
+			ret.SetGroupVersionKind(listKind)
+			return ret
+		})
+
+		destroyerFunc := forwardingregistry.DestroyerFunc(func() {})
+
+		getterFunc := forwardingregistry.GetterFunc(func(ctx context.Context, name string, options *metav1.GetOptions) (runtime.Object, error) {
+			clusterName, ok := schemaClusterAndNames[name]
+			if !ok {
+				return nil, errors.NewNotFound(resource.GroupResource(), name)
+			}
+
+			apiSchema, err := lister.Cluster(clusterName).Get(name)
+			if err != nil {
+				return nil, err
+			}
+
+			// Convert to unstructured
+			unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(apiSchema)
+			if err != nil {
+				return nil, fmt.Errorf("failed to convert schema to unstructured: %w", err)
+			}
+
+			result := &unstructured.Unstructured{Object: unstructuredObj}
+			result.SetGroupVersionKind(kind)
+			return result, nil
+		})
+
+		listerFunc := forwardingregistry.ListerFunc(func(ctx context.Context, options *metainternalversion.ListOptions) (runtime.Object, error) {
+			items := make([]unstructured.Unstructured, 0, len(schemaClusterAndNames))
+
+			for schemaName, clusterName := range schemaClusterAndNames {
+				apiSchema, err := lister.Cluster(clusterName).Get(schemaName)
+				if err != nil {
+					// Skip schemas that can't be found (they may have been deleted)
+					if errors.IsNotFound(err) {
+						continue
+					}
+					return nil, err
+				}
+
+				unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(apiSchema)
+				if err != nil {
+					return nil, fmt.Errorf("failed to convert schema %s to unstructured: %w", schemaName, err)
+				}
+
+				item := unstructured.Unstructured{Object: unstructuredObj}
+				item.SetGroupVersionKind(kind)
+				items = append(items, item)
+			}
+
+			list := &unstructured.UnstructuredList{Items: items}
+			list.SetGroupVersionKind(listKind)
+			return list, nil
+		})
+
+		watcherFunc := forwardingregistry.WatcherFunc(func(ctx context.Context, options *metainternalversion.ListOptions) (watch.Interface, error) {
+			// For now, return an empty watch that never sends events.
+			// A full implementation would need to watch the underlying schemas.
+			return watch.NewEmptyWatch(), nil
+		})
+
+		tableConvertorFunc := forwardingregistry.TableConvertorFunc(func(ctx context.Context, object runtime.Object, tableOptions runtime.Object) (*metav1.Table, error) {
+			if tableConvertor != nil {
+				return tableConvertor.ConvertToTable(ctx, object, tableOptions)
+			}
+			return rest.NewDefaultTableConvertor(resource.GroupResource()).ConvertToTable(ctx, object, tableOptions)
+		})
+
+		categoriesProviderFunc := forwardingregistry.CategoriesProviderFunc(func() []string {
+			return nil
+		})
+
+		resetFieldsStrategyFunc := forwardingregistry.ResetFieldsStrategyFunc(func() map[fieldpath.APIVersion]*fieldpath.Set {
+			return nil
+		})
+
+		// Return the storage struct matching what ProvideReadOnlyRestStorage returns
+		return &struct {
+			forwardingregistry.FactoryFunc
+			forwardingregistry.ListFactoryFunc
+			forwardingregistry.DestroyerFunc
+
+			forwardingregistry.GetterFunc
+			forwardingregistry.ListerFunc
+			forwardingregistry.WatcherFunc
+
+			forwardingregistry.TableConvertorFunc
+			forwardingregistry.CategoriesProviderFunc
+			forwardingregistry.ResetFieldsStrategyFunc
+		}{
+			FactoryFunc:     factoryFunc,
+			ListFactoryFunc: listFactoryFunc,
+			DestroyerFunc:   destroyerFunc,
+
+			GetterFunc:  getterFunc,
+			ListerFunc:  listerFunc,
+			WatcherFunc: watcherFunc,
+
+			TableConvertorFunc:      tableConvertorFunc,
+			CategoriesProviderFunc:  categoriesProviderFunc,
+			ResetFieldsStrategyFunc: resetFieldsStrategyFunc,
+		}, nil
+	}
+}
