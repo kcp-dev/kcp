9848b065ec261c75de35689c523926f0c24e6e69
Nelo-T. Wallus <n.wallus@sap.com>
2026-03-14 23:26:59 +0100

    Move BindRootAPI{,s} into own file
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- config/helpers/bootstrap.go
+++ config/helpers/bootstrap.go
@@ -42,12 +42,8 @@ import (
 	"k8s.io/client-go/restmapper"
 	"k8s.io/klog/v2"
 
-	apisv1alpha2 "github.com/kcp-dev/sdk/apis/apis/v1alpha2"
-	"github.com/kcp-dev/sdk/apis/core"
 	tenancyhelper "github.com/kcp-dev/sdk/apis/tenancy/v1alpha1/helper"
-	kcpclient "github.com/kcp-dev/sdk/client/clientset/versioned"
 
-	"github.com/kcp-dev/kcp/pkg/errgroup"
 	"github.com/kcp-dev/kcp/pkg/logging"
 )
 
@@ -226,72 +222,4 @@ func createResourceFromFS(ctx context.Context, client dynamic.Interface, mapper 
 	logging.WithObject(logger, upserted).Info("upserted object")
 
 	return nil
-}
-
-func BindRootAPIs(ctx context.Context, kcpClient kcpclient.Interface, exportNames ...string) error {
-	g := errgroup.WithContext(ctx)
-	for _, exportName := range exportNames {
-		g.Go(func(ctx context.Context) error {
-			if err := BindRootAPI(ctx, kcpClient, exportName); err != nil {
-				return fmt.Errorf("failed to bind root API %s: %w", exportName, err)
-			}
-			return nil
-		})
-	}
-	return g.Wait()
-}
-
-func BindRootAPI(ctx context.Context, kcpClient kcpclient.Interface, exportName string) error {
-	logger := klog.FromContext(ctx)
-
-	binding := &apisv1alpha2.APIBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: exportName,
-		},
-		Spec: apisv1alpha2.APIBindingSpec{
-			Reference: apisv1alpha2.BindingReference{
-				Export: &apisv1alpha2.ExportBindingReference{
-					Path: core.RootCluster.Path().String(),
-					Name: exportName,
-				},
-			},
-		},
-	}
-
-	created, err := kcpClient.ApisV1alpha2().APIBindings().Create(ctx, binding, metav1.CreateOptions{})
-	if err == nil {
-		logger := logging.WithObject(logger, created)
-		logger.V(2).Info("Created API binding")
-		return nil
-	}
-	if !apierrors.IsAlreadyExists(err) {
-		return err
-	}
-
-	return wait.PollUntilContextCancel(ctx, time.Second, true, func(ctx context.Context) (bool, error) {
-		existing, err := kcpClient.ApisV1alpha2().APIBindings().Get(ctx, exportName, metav1.GetOptions{})
-		if err != nil {
-			logger.Error(err, "error getting APIBinding", "name", exportName)
-			// Always keep trying. Don't ever return an error out of this function.
-			return false, nil
-		}
-
-		logger := logging.WithObject(logger, existing)
-		logger.V(2).Info("Updating API binding")
-
-		existing.Spec = binding.Spec
-
-		_, err = kcpClient.ApisV1alpha2().APIBindings().Update(ctx, existing, metav1.UpdateOptions{})
-		if err == nil {
-			return true, nil
-		}
-		if apierrors.IsConflict(err) {
-			logger.V(2).Info("API binding update conflict, retrying")
-			return false, nil
-		}
-
-		logger.Error(err, "error updating APIBinding")
-		// Always keep trying. Don't ever return an error out of this function.
-		return false, nil
-	})
 }
