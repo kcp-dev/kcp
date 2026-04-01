887144a8e887a9207dcf0cae609af9d4a5632b41
Lubomir I. Ivanov <neolit123@gmail.com>
2026-04-01 19:36:37 +0200

    sdk/testing: improve the RepositoryDir function and add tests
    The function is called from various locations but notably
    it can panic from test/e2e/framework/kcp.go if the sdk
    is located in the go build or mod cache.
    
    The main issue here is that taking the first caller and traversing
    up the tree may not find any .git files. This forces downstream
    users to have to vendor the sdk in their trees if they are calling
    kcp test utils such as creating a test server fixture.
    
    What this change does:
    - Add the KCP_REPO_DIR environment variable which the function
    just takes by default if its set, allowing the user to not rely
    on the traversal logic.
    - Takes into consideration the location of this file. This
    works for the monorepo case.
    - Traverse the call stack to find a location that is outside
    of the go caches. This works if the sdk module is published.
    - Traverse the candidate dirs to find the kcp go.mod file.
    - If the file was not found, error and mention that the user
    can set the env var.
    - Add unit tests for the function.
    
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- staging/src/github.com/kcp-dev/sdk/testing/helpers/repo.go
+++ staging/src/github.com/kcp-dev/sdk/testing/helpers/repo.go
@@ -17,41 +17,129 @@ limitations under the License.
 package helpers
 
 import (
-	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
 	goruntime "runtime"
+	"strings"
 )
 
-// RepositoryDir returns the absolute path of kcp-dev/kcp repository on disk.
+var (
+	readFile   = os.ReadFile
+	statIsDir  = statIsDirImpl
+	sourceFile = sourceFileImpl
+	frameFiles = frameFilesImpl
+	osStat     = os.Stat
+)
+
+// RepositoryDir attempts to find the kcp-dev/kcp repository on disk
+// by looking for its go.mod file. If the KCP_REPO_DIR environment variable is set,
+// its value will be returned directly as an absolute path.
 func RepositoryDir() (string, error) {
-	// Caller(0) returns the path to the calling test file rather than the path to this framework file. That
-	// precludes assuming how many directories are between the file and the repo root. It's therefore necessary
-	// to search in the hierarchy for an indication of a path that looks like the repo root.
-	_, sourceFile, _, _ := goruntime.Caller(0)
-	currentDir := filepath.Dir(sourceFile)
-	for {
-		// go.mod should always exist in the repo root
-		if _, err := os.Stat(filepath.Join(currentDir, ".git")); err == nil {
-			break
-		} else if errors.Is(err, os.ErrNotExist) {
+	const envVar = "KCP_REPO_DIR"
+
+	if repo := os.Getenv(envVar); repo != "" {
+		absRepo, isDir, err := statIsDir(repo)
+		if err != nil {
+			return "", fmt.Errorf("could not stat %s (%s): %w", envVar, repo, err)
+		}
+		if !isDir {
+			return "", fmt.Errorf("%s is not a directory: %s", envVar, repo)
+		}
+		return absRepo, nil
+	}
+
+	// Start with this source file's path. This works for the monorepo case -
+	// i.e. when this code is called from:
+	//   kcp-dev/kcp/staging/src/github.com/kcp-dev/sdk/testing/helpers/repo.go
+	var candidates []string
+	if f := sourceFile(); f != "" {
+		candidates = append(candidates, f)
+	}
+
+	// Also collect caller source files from the call stack that are not
+	// inside the go caches. This covers the case where the sdk repository
+	// is consumed as an external module dependency.
+	for _, f := range frameFiles() {
+		if !strings.Contains(f, filepath.Join("pkg", "mod")) &&
+			!strings.Contains(f, filepath.Join(".cache", "go-build")) {
+			candidates = append(candidates, f)
+		}
+	}
+
+	// Go through each candidate and try to find the kcp go.mod file in its tree.
+	for _, sourceFile := range candidates {
+		currentDir := filepath.Dir(sourceFile)
+		for {
+			modPath := filepath.Join(currentDir, "go.mod")
+			data, err := readFile(modPath)
+			if err == nil {
+				if strings.HasPrefix(strings.TrimSpace(string(data)), "module github.com/kcp-dev/kcp") {
+					return currentDir, nil
+				}
+			}
 			preDir := currentDir
 			currentDir, err = filepath.Abs(filepath.Join(currentDir, ".."))
 			if err != nil {
-				return "", err
+				break
 			}
 			if preDir == currentDir {
-				return "", errors.New("could not find kcp repository root")
+				break
 			}
-		} else {
-			return "", err
 		}
 	}
-	return currentDir, nil
+
+	return "", fmt.Errorf("could not find the kcp repository root; "+
+		"try setting the %s environment variable", envVar)
 }
 
 // RepositoryBinDir returns the absolute path of <repo-dir>/bin. That's where `make build` produces our binaries.
 func RepositoryBinDir() (string, error) {
 	repoDir, err := RepositoryDir()
 	return filepath.Join(repoDir, "bin"), err
+}
+
+// statIsDirImpl checks if the given path is a directory.
+func statIsDirImpl(path string) (string, bool, error) {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return "", false, err
+	}
+	stat, err := osStat(absPath)
+	if err != nil {
+		return "", false, err
+	}
+	return absPath, stat.IsDir(), nil
+}
+
+func sourceFileImplAnchor() {}
+
+// sourceFileImpl uses runtime.FuncForPC on a function in this file
+// to locate this source file location on disk.
+func sourceFileImpl() string {
+	fn := goruntime.FuncForPC(reflect.ValueOf(sourceFileImplAnchor).Pointer())
+	if fn == nil {
+		return ""
+	}
+	file, _ := fn.FileLine(fn.Entry())
+	return file
+}
+
+// frameFilesImpl returns a list of source files from the call stack frames.
+func frameFilesImpl() []string {
+	pcs := make([]uintptr, 64)
+	n := goruntime.Callers(0, pcs)
+	frames := goruntime.CallersFrames(pcs[:n])
+	var files []string
+	for {
+		frame, more := frames.Next()
+		if frame.File != "" {
+			files = append(files, frame.File)
+		}
+		if !more {
+			break
+		}
+	}
+	return files
 }
