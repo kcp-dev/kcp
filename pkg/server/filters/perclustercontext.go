c26a86cac1f138716775630ce66631a3a77fb8a1
Nelo-T. Wallus <n.wallus@sap.com>
2026-05-22 11:04:39 +0200

    Terser and more explicit connection context wrapping
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- pkg/server/filters/perclustercontext.go
+++ pkg/server/filters/perclustercontext.go
@@ -38,24 +38,35 @@ import (
 func WithPerClusterContext(handler http.Handler, mgr *contextmanager.Manager[logicalcluster.Path]) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		cluster := request.ClusterFrom(req.Context())
-		// Explicitly including wildcard requests. When a cluster is
-		// cancelled the wildcard connections must also be cancelled in
-		// case a wildcard watch targets objects in the affected logical
-		// cluster.
-		if cluster == nil || cluster.Name.Empty() || strings.HasPrefix(cluster.Name.String(), "system:") {
-			handler.ServeHTTP(w, req)
-			return
-		}
 
 		var clusterPath logicalcluster.Path
+		// Handling the differing cases in a switch to prevent crossing the logic.
 		switch {
+		case cluster == nil:
+			handler.ServeHTTP(w, req)
+			return
 		case cluster.Wildcard:
+			// Explicitly including wildcard requests. When a cluster is
+			// cancelled the wildcard connections must also be cancelled in
+			// case a wildcard watch targets objects in the affected logical
+			// cluster.
 			clusterPath = logicalcluster.Wildcard
+		case cluster.Name.Empty():
+			// .Name.Empty must be checked after .Wildcard as .Name will
+			// be empty if .Wildcard is true.
+			handler.ServeHTTP(w, req)
+			return
+		case strings.HasPrefix(cluster.Name.String(), "system:"):
+			// The per-shard system workspaces do not need a context as
+			// they can't be migrated and shouldn't be put in inactive
+			// state.
+			handler.ServeHTTP(w, req)
+			return
 		default:
 			clusterPath = cluster.Name.Path()
 		}
 
-		ctx, cleanup := mgr.ContextFor(req.Context(), clusterPath)
+		ctx, cleanup := mgr.Context(req.Context(), clusterPath)
 		defer cleanup()
 		handler.ServeHTTP(w, req.WithContext(ctx))
 	}
