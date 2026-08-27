b6fbb20fe7c04a0a021d56ab28a5583a55d231c9
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-08-27 07:59:31 +0200

    Add a maxmimum length for resource names to lessen estimated CEL cost
    127 because a resource can have up to 63 bytes per segment, so 63 for
    the resource, 63 for the subresource and 1 for the `/`.
    
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- staging/src/github.com/kcp-dev/sdk/apis/apis/v1alpha2/types_apiexport.go
+++ staging/src/github.com/kcp-dev/sdk/apis/apis/v1alpha2/types_apiexport.go
@@ -327,6 +327,7 @@ type GroupResource struct {
 	// not provided by an api export.
 	//
 	// +kubebuilder:validation:Pattern=`^[a-z][-a-z0-9]*[a-z0-9](/[a-z][-a-z0-9]*[a-z0-9])?$`
+	// +kubebuilder:validation:MaxLength=127
 	// +required
 	// +kubebuilder:validation:Required
 	Resource string `json:"resource"`
