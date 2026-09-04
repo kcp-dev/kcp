83f0609f5d67405241fd2fe966e7228e970b7390
Nelo-T. Wallus <n.wallus@sap.com>
2026-09-04 12:34:36 +0200

    Clearer validation message on permission claims for subresources
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- staging/src/github.com/kcp-dev/sdk/apis/apis/v1alpha2/types_apibinding.go
+++ staging/src/github.com/kcp-dev/sdk/apis/apis/v1alpha2/types_apibinding.go
@@ -84,7 +84,7 @@ type APIBindingSpec struct {
 // ScopedPermissionClaim embeds a PermissionClaim and adds a selector to
 // scope down access to objects of the claimed resource.
 //
-// +kubebuilder:validation:XValidation:rule="!self.resource.contains('/') || (has(self.selector.matchAll) && self.selector.matchAll)",message="subresource claims must use selector.matchAll"
+// +kubebuilder:validation:XValidation:rule="!self.resource.contains('/') || (has(self.selector.matchAll) && self.selector.matchAll)",message="for subresource claims the selector is ignored, scope is inherited from the parent resource claim"
 type ScopedPermissionClaim struct {
 	PermissionClaim `json:",inline"`
 
