eb930d99155feb3f666362d8fedaa1ac73d7c0c4
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-09-09 12:17:07 +0300

    Fix retry on informer lag

--- pkg/reconciler/apis/permissionclaimlabel/permissionclaimlabel_reconcile.go
+++ pkg/reconciler/apis/permissionclaimlabel/permissionclaimlabel_reconcile.go
@@ -282,13 +282,19 @@ func (c *controller) reconcile(ctx context.Context, apiBinding *apisv1alpha2.API
 			errsToDisplay,
 		)
 
-		return fmt.Errorf("%d error(s) applying permission claims for APIBinding %s|%s (showing the first %d): %w",
+		err := fmt.Errorf("%d error(s) applying permission claims for APIBinding %s|%s (showing the first %d): %w",
 			len(allErrs),
 			clusterName,
 			apiBinding.Name,
 			len(errsToDisplay.Errors()),
 			errsToDisplay,
 		)
+		if onlyInformerNotReady(allErrs) {
+			// nothing went wrong, the informers just aren't there yet:
+			// let the controller wait rather than back off.
+			return &informerNotReadyError{err: err}
+		}
+		return err
 	} else {
 		conditions.MarkTrue(apiBinding, apisv1alpha2.PermissionClaimsApplied)
 	}
@@ -323,10 +329,10 @@ func (c *controller) getInformerForGroupResource(group, resource string) (kcpkub
 	}
 	for _, gvr := range notSynced {
 		if gvr.Group == group && gvr.Resource == resource {
-			return nil, schema.GroupVersionResource{}, fmt.Errorf("informer for %s.%s has not synced yet", group, resource)
+			return nil, schema.GroupVersionResource{}, fmt.Errorf("%w for %s.%s: not synced yet", errInformerNotReady, group, resource)
 		}
 	}
-	return nil, schema.GroupVersionResource{}, fmt.Errorf("unable to find informer for %s.%s", group, resource)
+	return nil, schema.GroupVersionResource{}, fmt.Errorf("%w for %s.%s", errInformerNotReady, group, resource)
 }
 
 func (c *controller) patchGenericObject(ctx context.Context, obj metav1.Object, gvr schema.GroupVersionResource, lc logicalcluster.Path) error {
